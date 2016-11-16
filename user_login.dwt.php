a66eebe582f1ca1f52414d3380d6b6ea928dd1e3
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-16 16:09:43 +0800

    修改登陆页面

--- user_login.dwt.php
+++ user_login.dwt.php
@@ -25,8 +25,9 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	</div>
 	<div class="form-group">
 		<label class="input">
-			<i class="iconfont icon-lock"></i>
-			<input placeholder="{$lang.input_passwd}"  name="password" type="password" datatype="*6-16" errormsg="密码错误请重新输入！" autocomplete="off" />
+			<i class="iconfont icon-lock "></i>
+			<i class="iconfont icon-attention icon-left"></i>
+			<input placeholder="{$lang.input_passwd}" name="password" type="password" datatype="*6-16" errormsg="密码错误请重新输入！" autocomplete="off" />
 		</label>
 	</div>
 	<!-- 判断是否启用验证码{if $enabled_captcha} -->
@@ -38,8 +39,8 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 		</label>
 	</div>
 	<!--{/if}-->
-	<div class="ecjia-login-b ecjia-login-login-foot">
-		<a class="ecjiaf-fl ecjia-margin-l" href="{url path='user/index/register'}">注册新账号</a>
+	<div class="ecjia-login-b ecjia-login-login-foot margin-right-left">
+<!-- 		<a class="ecjiaf-fl ecjia-margin-l" href="{url path='user/index/register'}">注册新账号</a> -->
 		<a class="ecjiaf-fr ecjia-margin-r" href="{url path='index/get_password_email'}">{$lang.forgot_password}?</a>
 		<!-- {if $anonymous_buy eq 1 && $step eq 'flow'} 是否允许未登录用户购物 -->
 		<a class="ecjiaf-fr ecjia-margin-r" href="{url path='cart/flow/consignee' args="direct_shopping=1"}">{$lang.direct_shopping}</a>
@@ -50,6 +51,9 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
             <input type="hidden" name="referer" value="{$smarty.get.referer}" />
             <input type="submit" class="btn btn-info login-btn" value="{$lang.login}" />
 	    </div>	
+	</div>
+	<div class="ecjia-login-b">
+
 	</div>
 </form>
 <!-- {/block} -->
