9cb780d3eb25a741575def2f35034d0c17b635ff
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 19:33:55 +0800

    新增定位页面

---
+++ user_location.dwt.php
@@ -0,0 +1,57 @@
+<?php 
+/*
+Name: 定位列表模板
+Description: 定位列表页
+Libraries: page_menu,page_header
+*/
+defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
+?>
+<!-- {extends file="ecjia-touch.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">ecjia.touch.region_change();</script>
+<!-- {/block} -->
+
+<!-- {block name="main-content"} -->
+<!-- #BeginLibraryItem "/library/page_header.lbi" -->
+<!-- #EndLibraryItem -->
+
+<form class="ecjia-form ecjia-margin-t" name="theForm" action="{url path='user/user_address/inster_addres'}" method="post">
+	<div class="form-group form-group-text">
+		<label class="input">
+			<span>所在地区： </span>
+			<input name="district" placeholder="{t}请选择城市{/t}" type="text" datatype="*" value="" />
+			<label class="iconfont icon-jiantou-right"></label>
+		</label>
+	</div>
+	<div class="form-group form-group-text">
+		<label class="input">
+			<span class="ecjiaf-fl">收货地址： </span>
+			<input name="location" placeholder="{t}写字楼，小区，学校，街道{/t}" type="text" datatype="*" value="" />
+			<label class="iconfont icon-focus"></label>
+		</label>
+	</div>
+	<div class="form-group form-group-text">
+		<label class="input">
+			<input name="address" placeholder="{t}楼层，门牌{/t}" type="text" datatype="*" value="{$consignee.address|escape}" />
+		</label>
+	</div>
+	<div class="form-group form-group-text">
+		<label class="input">
+			<span class="ecjiaf-fl">收货姓名： </span>
+			<input name="consignee" placeholder="{t}请输入真实姓名，限6个字{/t}" type="text" value="{$consignee.consignee|escape}" datatype="*1-15" errormsg="请输入正确格式联系人" />
+		</label>
+	</div>
+	<div class="form-group form-group-text">
+		<label class="input">
+			<span class="ecjiaf-fl">收货电话： </span>
+			<input name="mobile" placeholder="{t}请确保收货电话真实有效{/t}" type="tel" value="{$consignee.mobile|escape}" datatype="m" errormsg="请输入正确格式的联系方式" />
+		</label>
+	</div>
+	<div class="ecjia-margin-t ecjia-margin-b">
+		<input class="btn btn-info nopjax" name="submit" type="submit" value="{t}保存{/t}"/>
+		<input name="address_id" type="hidden" value="{$consignee.address_id}" />
+	</div>
+</form>
+
+<!-- {/block} -->
