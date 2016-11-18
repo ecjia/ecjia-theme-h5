909fdb804bef86aa6e9400e3dc4b5ad43fb8ab2d
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 11:23:13 +0800

    修改用户中心样式

--- user_profile.dwt.php
+++ user_profile.dwt.php
@@ -11,36 +11,33 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- {block name="main-content"} -->
 <!-- #BeginLibraryItem "/library/page_header.lbi" -->
 <!-- #EndLibraryItem -->
-<div class="ecjia-user-info ecjia_user-info-edit ecjia-margin-b">
-	<div class="user-img"><img src="{$user_img}"></div>
-	<a href="{url path='touch/index/download'}">{t}如需修改头像请下载APP{/t}</a>
+<div class="ecjia-login">
+	<div class="user-img">
+	   <img src="{$user_img}">
+	</div>
 </div>
-<form class="ecjia-form user-profile-form" name="user_profile" action="{url path='user/user_profile/update_profile'}" method="post">
-	<div class="form-group form-group-text">
+<form class="ecjia-form ecjia-login-user-profile-form" name="user_profile" action="{url path='user/user_profile/update_profile'}" method="post">
+	<div class="form-group form-group-text edit-password">
 		<label class="input">
-			<span>{$lang.email}</span>
-			<input name="email" type="email" placeholder="{$lang.no_emaill}"  value="{$profile.email}" datatype="e" errormsg="请输入正确格式的邮箱" />
+			<span>用户名 </span>
+			<input name="district" placeholder="{t}请输入用户名{/t}" type="text" href="{url path='index/logout'}" datatype="*" value=""/>
+			<label class="iconfont icon-jiantou-right"></label>
 		</label>
 	</div>
-	<!-- {foreach from=$extend_info_list item=field} -->
-	<!-- {if $field.id neq 6} -->
 	<div class="form-group form-group-text">
 		<label class="input">
-			<span>{$field.reg_field_name}</span>
-			<input name="extend_field{$field.id}" type="text" value="{$field.content}" placeholder="{$field.reg_field_name}" />
+			<span>{t}用户等级{/t}</span>
+			<input name="email" type="email" placeholder="{$lang.no_emaill}"  value="{$profile.name}" datatype="e" errormsg="请输入正确格式的邮箱" />
 		</label>
 	</div>
-	<!-- {/if} -->
-	<!-- {/foreach} -->
 	<div class="edit-password ecjia-margin-t">
 		<a href="{url path='user/user_profile/edit_password'}">
-			修改密码
-			<i class="iconfont ecjiaf-fr icon-jiantou-right"></i>
+			<span>{t}修改密码{/t}</span>
+			<label class="iconfont icon-jiantou-right a" ></label>
 		</a>
 	</div>
-	<input name="act" type="hidden" value="profile" />
-	<div class="ecjia-margin-t ecjia-margin-b">
-		<input class="btn btn-info" name="submit" type="submit" value="{$lang.confirm_edit}" />
+    <div class="ecjia-margin-t ecjia-login-margin-top">
+		<a class="btn btn-info nopjax" href="{url path='index/logout'}">{t}退出登录{/t}</a>
 	</div>
 </form>
 <!-- {/block} -->
