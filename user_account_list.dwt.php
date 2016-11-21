4cf3ed15968bd2b6eacc837c991cf92751c1393a
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-21 13:45:05 +0800

    调整钱包样式

--- user_account_list.dwt.php
+++ user_account_list.dwt.php
@@ -12,11 +12,6 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- #BeginLibraryItem "/library/page_header.lbi" -->
 <!-- #EndLibraryItem -->
 
-<ul class="ecjia-list ecjia-list-three user_account_list-nav ecjia-nav">
-	<li{if $status eq ''} class="active"{/if}><a data-rh="1" href="{url path='user/user_account/account_list'}">{t}全部{/t}</a></li>
-	<li{if $status eq 'recharge'} class="active"{/if}><a data-rh="1" href="{url path='user/user_account/account_list' args='status=recharge'}">{t}充值{/t}</a></li>
-	<li{if $status eq 'withdraw'} class="active"{/if}><a data-rh="1" href="{url path='user/user_account/account_list' args='status=withdraw'}">{t}支出{/t}</a></li>
-</ul>
 <ul class="ecjia-list ecjia-account-list">
 	<!--{foreach from=$account_log item=item}-->
 	<li class="{if $item.amount gt 0} recharge {else} carries {/if}">
@@ -34,5 +29,9 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 		<p>{t}暂无账单记录{/t}</p>
 	</div>
 	<!--{/foreach}-->
+	<div class="two-btn account-btn">
+		<input class="btn btn-recharge" name="submit" type="submit" value="{t}充值{/t}" />
+		<input class="btn btn-recharge" name="submit" type="submit" value="{t}提现{/t}" />
+	</div>
 </ul>
 <!-- {/block} -->
