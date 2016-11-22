9c1420466d4a67f64ad328d1ee1f90ab6141a947
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-22 14:18:59 +0800

    修改红包列表样式

--- user_bonus.dwt.php
+++ user_bonus.dwt.php
@@ -33,7 +33,7 @@ var bonus_sn_empty = '{$lang.bonus_sn_empty}';
 <!-- {block name="ajaxinfo"} -->
 	<!--{foreach from=$bonus item=item}-->
 		<li class="ecjia-margin-b list-l-size">
-			<div class="{if $item.status eq '未使用'}user-bonus-head{else}user-bonus-head-expired{/if}">
+			<div class="user-bonus-info {if $item.status eq '未使用'}user-bonus-head{else}user-bonus-head-expired{/if}">
 				<div class="type-l {if $item.status eq '未使用'}no-type-money{else}type-money{/if}">￥20.00</div>
 				<div class="type-r">
 					<p class="type-name">{$item.type_name}</p>
