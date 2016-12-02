67e668806ebf228ab407416773d0a3031a57dcee
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-02 09:57:11 +0800

    修改删除地址、我的位置

--- user_address_list.dwt.php
+++ user_address_list.dwt.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Name: 收货地址列表模板
 Description: 收货地址列表页
@@ -7,6 +7,12 @@ Libraries: page_menu,page_header
 defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 ?>
 <!-- {extends file="ecjia-touch.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">ecjia.touch.more();</script>
+<script type="text/javascript">ecjia.touch.delete_list_click();</script>
+<script type="text/javascript">ecjia.touch.asynclist();</script>
+<!-- {/block} -->
 
 <!-- {block name="main-content"} -->
 
@@ -19,8 +25,8 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	</div>
 	<section>
 	<!-- {if $address_list} -->
-		<ul class="ecjia-list list-one" id="J_ItemList">			
-			<!-- 配送地址 start--> 
+		<ul class="ecjia-list list-one" id="J_ItemList">
+			<!-- 配送地址 start-->
 			<!-- {foreach from=$address_list item=value} 循环地址列表 -->
 			<li>
 				<div>
@@ -28,15 +34,18 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 					<p class="ecjiaf-fl ecjia-margin-l ecjia-address-mobile">{$value.mobile}</p>
 				</div>
 				<br />
-				<div class="address ecjiaf-wwb">{$value.province_name}{$value.city_name}{$value.address}{$value.address_info}</div>	
+				<div class="address ecjiaf-wwb">{$value.province_name} {$value.city_name} {$value.address} {$value.address_info}</div>
 				<hr />
 				<!-- {if $value.default_address eq 1} -->
 				<p class="is-default"><i class="iconfont icon-roundcheckfill"></i> {t}设为默认{/t}</p>
 				<!-- {else} -->
 				<p class="not-default"><i class="iconfont icon-round"></i> {t}设为默认{/t}</p>
 				<!-- {/if} -->
+				
 				<a href="{RC_uri::url('user/user_address/edit_address')}&id={$value.id}"><i class="iconfont icon-bianji1">{t}编辑{/t}</i></a>
-				<a class="nopjax" href="javascript:;" data-toggle="del_list" data-url="{url path='user/user_address/del_address_list'}" data-id="{$value.id}" data-msg="{t}你确定要删除此收货地址吗？{/t}"><i class="iconfont icon-delete">{t}删除{/t}</i></a>
+				
+			    <a class="nopjax" href="javascript:;" data-toggle="del_list" data-url="{url path='user/user_address/del_address_list'}" data-id="{$value.id}" data-msg="{t}你确定要删除此收货地址吗？{/t}"><i class="iconfont icon-delete">{t}删除{/t}</i></a>
+		
 			</li>
 			<!-- {/foreach} -->
 			<!-- 配送地址end-->
