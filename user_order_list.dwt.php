0337c8cf93b0a8d7e6b3101ae813b075035d5454
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-02 18:00:44 +0800

    订单列表 状态链接修改

--- user_order_list.dwt.php
+++ user_order_list.dwt.php
@@ -98,10 +98,10 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- {foreach from=$order_list item=list} -->
 		<li class="ecjia-order-item ecjia-checkout ecjia-margin-t">
 			<div class="order-hd">
-				<a class="ecjiaf-db" href='{url path="goods/category/store_goods" args="store_id={$list.seller_id}"}'>
+				<a class="ecjiaf-fl" href='{url path="goods/category/store_goods" args="store_id={$list.seller_id}"}'>
 					<i class="iconfont icon-shop"></i>{$list.seller_name} <i class="iconfont icon-jiantou-right"></i>
-					<span class="ecjiaf-fr ecjia-color-green">{$list.label_order_status}</span>
 				</a>
+				<a class="ecjiaf-fr" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}"}'><span class="ecjia-color-green">{$list.label_order_status}</span></a>
 			</div>
 			<div class="flow-goods-list">
 				<a class="ecjiaf-db" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}"}'>
