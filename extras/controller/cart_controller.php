a96017dd6216df05a1be71726464e686ae4ca87b
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-06 16:15:46 +0800

    支付结算分离

--- extras/controller/cart_controller.php
+++ extras/controller/cart_controller.php
@@ -1309,8 +1309,6 @@ class cart_controller {
     //     ecjia_front::$controller->assign_title(RC_Lang::lang('order_submit'));
     //     ecjia_front::$controller->assign_lang();
     
-        $order_id = !empty($_GET['order_id']) ? intval($_GET['order_id']) : 0;
-        if (!$order_id) {
             $address_id = empty($_POST['address_id']) ? 0 : intval($_POST['address_id']);
             $rec_id = empty($_POST['rec_id']) ? 0 : trim($_POST['rec_id']);
             $pay_id = empty($_POST['pay_id']) ? 0 : intval($_POST['pay_id']);
@@ -1346,84 +1344,7 @@ class cart_controller {
             }
 //             $_SESSION['cart']['order_info'][$rs['data']['order_id']] = $rs['data'];
             $order_id = $rs['data']['order_id'];
-        } else {
-//             $rs['data'] = $_SESSION['cart']['order_info'][$order_id];
-
-            $pay_id = !empty($_GET['pay_id']) ? intval($_GET['pay_id']) : 0;
-            $pay_code = !empty($_GET['pay_code']) ? trim($_GET['pay_code']) : 0;
-            
-            if ($pay_id && $pay_code) {
-                //修改支付方式，更新订单
-                $params = array(
-                    'token' => ecjia_touch_user::singleton()->getToken(),
-                    'order_id' => $order_id,
-                    'pay_id' => $pay_id,
-                );
-                $rs_update = ecjia_touch_manager::make()->api(ecjia_touch_api::ORDER_UPDATE)->data($params)
-                ->send()->getBody();
-                $rs_update = json_decode($rs_update,true);
-                if (! $rs_update['status']['succeed']) {
-                    ecjia_front::$controller->showmessage($rs_update['status']['error_desc'], ecjia::MSGSTAT_ERROR | ecjia::MSGTYPE_ALERT);
-                }
-            }
-            
-        }
-        
-        //获得订单支付信息
-        $params = array(
-                'token' => ecjia_touch_user::singleton()->getToken(),
-                'order_id' => $order_id,
-            );
-        $rs_pay = ecjia_touch_manager::make()->api(ecjia_touch_api::ORDER_PAY)->data($params)
-        ->send()->getBody();
-        $rs_pay = json_decode($rs_pay,true);
-        if (! $rs_pay['status']['succeed']) {
-            ecjia_front::$controller->showmessage($rs_pay['status']['error_desc'], ecjia::MSGSTAT_ERROR | ecjia::MSGTYPE_ALERT);
-        }
-        if ($rs_pay['data']['payment']['error_message']) {
-            ecjia_front::$controller->assign('pay_error', $rs_pay['data']['payment']['error_message']);
-        }
-        
-        $order = $rs_pay['data']['payment'];
-        
-        _dump($order);
-        $need_other_payment = 0;
-        if ($order ['pay_code'] == 'pay_balance') {
-            if ($rs_pay['data']['payment']['error_message']) {
-                $need_other_payment = 1;
-            }
-        } else {
-            //其他支付方式
-            $need_other_payment = 1;
-        }
-        
-        if ($need_other_payment) {
-            $params = array(
-                'token' => ecjia_touch_user::singleton()->getToken(),
-            );
-            $rs_payment = ecjia_touch_manager::make()->api(ecjia_touch_api::SHOP_PAYMENT)->data($params)
-            ->send()->getBody();
-            $rs_payment = json_decode($rs_payment,true);
-            
-            if (! $rs_payment['status']['succeed']) {
-                ecjia_front::$controller->showmessage($rs_payment['status']['error_desc'], ecjia::MSGSTAT_ERROR | ecjia::MSGTYPE_ALERT);
-            }
-            $payment_list = touch_function::change_array_key($rs_payment['data']['payment'], 'pay_code');
-//             _dump($payment_list);
-            unset($payment_list[$order['pay_code']]);
-            _dump($payment_list);
-            //过滤当前支付方式
-//             foreach
-            ecjia_front::$controller->assign('payment_list', $payment_list);
-        }
-        _dump($rs_pay);
-        
-        $order['formated_order_amount'] = price_format($order['order_amount']);
-        $order['order_id'] = $order_id;
-        ecjia_front::$controller->assign('data', $order);
-        ecjia_front::$controller->assign('pay_online', $order['pay_online']);
-        
-        ecjia_front::$controller->display('flow_done.dwt');
+            ecjia_front::$controller->redirect(RC_Uri::url('pay/index/init', array('order_id' => $order_id, 'tips_show' => 1)));
     }
 
     /**
---
+++ pay.dwt.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Name: 支付结算模板
+Description: 
+Libraries: page_menu,page_header
+*/
+defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
+?>
+<!-- {extends file="ecjia-touch.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<!-- {/block} -->
+
+<!-- {block name="main-content"} -->
+<!-- #EndLibraryItem -->
+<div class="ecjia-flow-done">
+    {if $tips_show}
+    <div class="flow-success">
+        <p>恭喜您，订单已经生成~</p>
+    </div>
+    {/if}
+    <ul class="ecjia-list ecjia-margin-t">
+        <li>应付金额：<span class="ecjiaf-fr">{if $data.formated_order_amount}{$data.formated_order_amount}{else}{$data.order_amount}{/if}</span></li>
+        <li>支付方式：<span class="ecjiaf-fr flow-msg">{$data.pay_name}</span></li>
+    </ul>
+    <div class="ecjia-margin-t ecjia-margin-b flow-msg">{$pay_error}</div>
+    {if $payment_list}
+    <ul class="ecjia-list ecjia-margin-t">
+        <li>
+                        其它支付方式 <span class="ecjiaf-fr"><i class="iconfont icon-jiantou-bottom"></i></span>
+        </li>
+    </ul>
+    <ul class="ecjia-list list-short payment-list">
+    <!-- {foreach from=$payment_list item=list} -->
+        <li>
+            <span class="icon-name" data-code="{$list.pay_code}"><a href='{url path="pay/index/init" args="order_id={$data.order_id}&pay_id={$list.pay_id}&pay_code={$list.pay_code}"}'>{$list.pay_name}</a></span>
+        </li>
+    <!-- {/foreach} -->
+    </ul>
+    {/if}
+
+    {if $pay_online}
+    <div class="ecjia-margin-t ecjia-margin-b">
+        <a class="btn" href="{$pay_online}">确认支付</a>
+    </div>
+    {/if}
+    
+    <div class="ecjia-margin-t ecjia-margin-b two-btn">
+        <a class="btn" href='{url path="touch/index/init"}'>去购物</a>
+        <a class="btn" href='{url path="user/user_order/order_detail" args="order_id={$data.order_id}"}'>查看订单</a>
+    </div>
+</div>
+
+
+<!-- {/block} -->
