b162a5eca7645779af2938bcb9eb697438c6902e
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-08 18:15:17 +0800

    新增我的奖励

--- user_my_reward.dwt.php
+++ user_my_reward.dwt.php
@@ -10,7 +10,16 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 
 <!-- {block name="main-content"} -->
 
-<div class="ecjia-user ecjia-user-head">
-    我的奖励
+<div class="ecjia-spread ecjia-margin-t">
+    <div class="ecjia-floor ecjia-bottom-bar-pannel">
+        <ul class="tab5">
+            <li>
+                <div>111</div>
+            </li>
+            <li>
+                <div>111</div>
+            </li>
+        </ul>
+    </div>
 </div>
 <!-- {/block} -->
