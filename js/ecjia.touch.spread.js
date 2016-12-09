6f45469390e7ab545a0a982b7925263e6b3e049e
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-09 17:21:11 +0800

    我的奖励页

--- js/ecjia.touch.spread.js
+++ js/ecjia.touch.spread.js
@@ -5,11 +5,29 @@
 	ecjia.touch.spread = {
 		init : function(){
 			ecjia.touch.spread.spread();
+			ecjia.touch.spread.hint();
 		},
 		spread: function() {
 			$('.would-spread').on('click', function (e) {
 				//$('.bottom-elastic-layer').animate({height:'toggle'});
 			});
-        },      
+        },  
+		hint: function() {
+			var myApp = new Framework7({
+				modalButtonOk: '确定',
+				modalTitle: '温馨提示',
+		    });
+			var $$ = Dom7;
+			
+			$$('.alert-text1').on('click', function () {
+			    myApp.alert('邀请成功即可获得积分奖励' + '<br>' + '积分可在购买商品时使用');
+			});
+			$$('.alert-text2').on('click', function () {
+			    myApp.alert('邀请成功即可获得红包奖励' + '<br>' + '红包可在购买商品时使用');
+			});
+			$$('.alert-text3').on('click', function () {
+			    myApp.alert('邀请成功即可获得现金奖励' + '<br>' + '现金可在购买商品时使用');
+			});
+        },     
 	};
 })(ecjia, jQuery);
--- user_my_reward.dwt.php
+++ user_my_reward.dwt.php
@@ -8,23 +8,68 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 ?>
 <!-- {extends file="ecjia-touch.dwt.php"} -->
 
-<!-- {block name="main-content"} -->
+<!-- {block name="footer"} -->
+<script type="text/javascript">ecjia.touch.spread.init();</script>
+<!-- {/block} -->
 
-<div class="ecjia-spread ecjia-margin-t">
+<!-- {block name="main-content"} -->
+<div class="ecjia-spread-pb">
+    <div class="ecjia-spread ecjia-margin-b">
+        <ul class="ecjia-list list-short">
+            <li>
+                <div class="reward-title">获得的积分奖励</div>
+                <div class="reward-num1">0</div>
+                <div class="reward-hint">
+                    <a href="#" class="alert-text1">
+                        <img src="./images/wallet/60x60_3g.png">
+                    </a>
+                </div>
+                <div class="reward-shadow">
+                    <img src="./images/wallet/220x30.png">
+                </div>
+            </li>
+            <li>
+                <div class="reward-title">获得的红包奖励</div>
+                <div class="reward-num2">0</div>
+                <div class="reward-hint">
+                    <a href="#" class="alert-text2">
+                        <img src="./images/wallet/60x60_3b.png">
+                    </a>
+                </div>
+                <div class="reward-shadow">
+                    <img src="./images/wallet/220x30.png">
+                </div>
+            </li>
+            <li>
+                <div class="reward-title">获得的现金奖励</div>
+                <div class="reward-num3">0.00</div>
+                <div class="reward-hint">
+                    <a href="#" class="alert-text3">
+                        <img src="./images/wallet/60x60_4o.png">
+                    </a>
+                </div>
+                <div class="reward-shadow">
+                    <img src="./images/wallet/220x30.png">
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="ecjia-spread">
     <div class="ecjia-bottom-bar-pannel">
         <ul class="tab5">
             <li>
                 <a href="{url path='user/user_bonus/reward_detail'}">
                     <div class="qrcode_image2">
-                        <img src="./images/wallet/50_5.png">
+                        <img src="./images/wallet/60x60_1.png">
                     </div>
                     <span>奖励明细</span>
                 </a>
             </li>
             <li>
                 <a href="{url path='user/user_bonus/get_integral'}">
-                    <div class="qrcode_image2">
-                        <img src="./images/wallet/50_5.png">
+                    <div class="qrcode_image3">
+                        <img src="./images/wallet/60x60_2.png">
                     </div>
                     <span>赚积分</span>
                 </a>
