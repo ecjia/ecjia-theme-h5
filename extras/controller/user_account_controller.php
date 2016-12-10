bae158205298d7150a7ebcf8891d990d51b9808f
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-10 17:35:04 +0800

    修改交易详情样式

--- extras/controller/user_account_controller.php
+++ extras/controller/user_account_controller.php
@@ -126,7 +126,14 @@ class user_account_controller {
     	        $arr[$time][$key]['add_time'] = substr($val['add_time'],5,11);
     	    }
     	}
+    	foreach ($arr as $key => $val) {
+            ecjia_front::$controller->assign('key'.$key, $key);    	    
+    	}
     	$user_img = RC_Theme::get_template_directory_uri().'/images/user_center/icon-login-in2x.png';
+    	$user = ecjia_touch_manager::make()->api(ecjia_touch_api::USER_INFO)->run();
+    	if (!empty($user['avatar_img'])) {
+    	    $user_img = $user['avatar_img'];
+    	}
     	ecjia_front::$controller->assign('user_img', $user_img);
     	ecjia_front::$controller->assign('type', $type);
     	ecjia_front::$controller->assign('now_mon', $now_mon);
@@ -138,7 +145,6 @@ class user_account_controller {
     	    $more = 1;
     	}
     	return ecjia_front::$controller->showmessage('success', ecjia::MSGSTAT_SUCCESS | ecjia::MSGTYPE_JSON, array('list' => $sayList, 'page', 'is_last' => $more));
-
     }
 
     /**
