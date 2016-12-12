a9c3aa229337e4ccd4e18e18c4c5ae03713121e5
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-12 12:43:54 +0800

    修改明细页

--- js/ecjia.touch.spread.js
+++ js/ecjia.touch.spread.js
@@ -18,13 +18,14 @@
 				modalTitle: '温馨提示',
 		    });
 			var $$ = Dom7;
-			
+			var length = parseInt($('.swiper-wrapper').children('div').length) - 1;
 			var mySwiper3 = myApp.swiper('.swiper-3', {
 				  pagination:'.swiper-3 .swiper-pagination',
 				  spaceBetween: 10,
 				  effect : 'coverflow',
 				  slidesPerView: 3,
 				  centeredSlides: true,
+				  initialSlide : length,
 				  coverflow: {
 			            rotate: 0,
 			            stretch: 3,
@@ -33,16 +34,29 @@
 			            slideShadows : false
 			        },
 			        onClick : function(swiper){
+			        	$(".detail-list").html(' ');
+			        	$(".detail-list").attr('data-url', '');
+						$(".detail-list").attr('data-toggle', '');
+			        	
 			        	var index = swiper.clickedIndex;
 			        	var date = $('.swiper-wrapper').children('.swiper-slide').eq(index).children('span').attr('data-date');
-			        	console.log(date);
 			        	var url = $('input[name="reward_url"]').val();
 			        	var info = {
 			        		'date' : date
 			        	};
-						$.post(url, info, function(data){
-							return false;
-							$(".data-date").html(data);
+						$.get(url, info, function(data){
+							$(".detail-list").attr('data-url', data.data.url);
+							$(".detail-list").attr('data-toggle', data.data.data_toggle);
+							$(".detail-list").html(' ').html(data.list);
+							$('.load-list').remove();
+							
+							if (data.list == null && parseInt($('.detail-list').children('li').length) == 0) {
+								var empty = '<div class="ecjia-nolist">' + 
+								'<div class="img-noreward"></div>' + 
+								'<p>暂无奖励</p></div>';
+								$(".detail-list").html(empty);
+							}
+							ecjia.touch.asynclist();
 						});
 			        }
 				});
