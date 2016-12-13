bf9e7f76f678228f4b106201ea358c408152eb3b
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-13 14:35:47 +0800

    去除下载

--- library/page_header.lbi.php
+++ library/page_header.lbi.php
@@ -5,19 +5,6 @@ Description: 这是公共页头文件
 */
 defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 ?>
-<!-- {if $smarty.cookies.hide_download neq 1 && $shop_app_icon } -->
-<script type="text/javascript">ecjia.touch.close_app_download();</script>
-<div class="ecjia-app-download">
-    <div class="ecjiaf-fl"><i class="iconfont icon-close"></i></div>
-    <div class="ecjia-app-download-bd">
-        <img class="shop_app_icon" src="{$shop_app_icon}" alt="ECJia-touch">
-        <p>享受流畅的商城</p>
-        <p>快下载ECJiaAPP</p>
-    </div>
-    <a class="btn btn-info download-btn" href="{url path='touch/index/download'}">下载</a>
-</div>
-<!-- {/if} -->
-
 <header class="ecjia-header">
 	<div class="ecjia-header-left">
 	</div>
