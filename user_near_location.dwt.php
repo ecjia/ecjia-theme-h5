8df63cb43f826a8d9f64ce36fbe8149c6b4c2638
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-13 15:13:39 +0800

    更新选择城市

--- user_near_location.dwt.php
+++ user_near_location.dwt.php
@@ -22,7 +22,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 {/if}
     <div class="ecjia-zs">
       <div class="ecjia-zt al">
-              <a href="{url path='user/user_address/city' args="city=selectcity"}"><h2 class="ecjia-zu">{if $smarty.get.city}{$smarty.get.city}{/if}</h2></a>
+              <a href="{url path='user/user_address/city' args="city=selectcity{if $smarty.get.city_id}&city_id={$smarty.get.city_id}{/if}"}"><h2 class="ecjia-zu">{if $smarty.get.city}{$smarty.get.city}{/if}</h2></a>
               <input id="list" data-toggle="search-address" data-url="{url path='user/user_address/near_address'}"  class="ecjia-zv" name="address" type="text" placeholder="小区、写字楼、学校" maxlength="50" >
       </div>
       <div class="ecjia-list">
