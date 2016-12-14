e0bc9b76f51e3fbc804c6f88ac42b35c30fac3a9
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-14 17:10:01 +0800

    更新搜索关键词获取列表  分离控制器

---
+++ search_location.dwt.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Name: 关键词搜索地址
+Description: 关键词搜索地址
+Libraries: page_menu,page_header
+*/
+defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
+?>
+<!-- {extends file="ecjia-touch.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">ecjia.touch.address_list();</script>
+<!-- {/block} -->
+
+<!-- {block name="main-content"} -->
+<div id="ecjia-zs" data-url="{url path='touch/index/init'}">
+    <div class="ecjia-zs">
+      <div class="ecjia-zt al">
+           <a href="{url path='user/user_address/city' args="city=location_select_city{if $smarty.get.city_id}&city_id={$smarty.get.city_id}{/if}"}">
+             	<h2 class="ecjia-zu">{if $smarty.get.city}{$smarty.get.city}{/if}</h2>
+           </a>
+           <input id="list" data-toggle="search-address" data-url="{url path='location/index/search_list'}"  class="ecjia-zv" name="address" type="text" placeholder="小区、写字楼、学校" maxlength="50" >
+      </div>
+      <div class="ecjia-address_list">
+           <ul class="nav-list-ready"></ul>    
+      </div>
+</div>
+<!-- {/block} -->
