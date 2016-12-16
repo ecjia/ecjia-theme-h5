f45668199d5c2121269d6842af4a12c78ce36b35
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-16 19:01:57 +0800

    支付方式 分离线上线下

--- flow_pay.dwt.php
+++ flow_pay.dwt.php
@@ -17,8 +17,10 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- #EndLibraryItem -->
 <form id="theForm" name="theForm" action='{url path="cart/flow/checkout" args="address_id={$address_id}&rec_id={$rec_id}"}' method="post">
     <div class="ecjia-select">
-        <ul class="ecjia-list ecjia-margin-t">
-            <!-- {foreach from=$payment_list item=rs} -->
+        {if $payment_list.online}
+        <p class="select-title ecjia-margin-l">线上支付</p>
+        <ul class="ecjia-list">
+            <!-- {foreach from=$payment_list.online item=rs} -->
             <label class="select-item" for="{$rs.pay_code}">
                 <li>
                     <span class="slect-title">{$rs.pay_name}</span>
@@ -30,6 +32,23 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
             </label>
             <!-- {/foreach} -->
         </ul>
+        {/if}
+        {if $payment_list.offline}
+        <p class="select-title ecjia-margin-l">线下支付</p>
+        <ul class="ecjia-list">
+            <!-- {foreach from=$payment_list.offline item=rs} -->
+            <label class="select-item" for="{$rs.pay_code}">
+                <li>
+                    <span class="slect-title">{$rs.pay_name}</span>
+                    <span class="ecjiaf-fr">
+                        <input type="radio" id="{$rs.pay_code}" name="payment" value="{$rs.pay_id}" {if $smarty.get.pay_id eq $rs.pay_id}checked="true"{/if}>
+                        <label for="{$rs.pay_code}"></label>
+                    </span>
+                </li>
+            </label>
+            <!-- {/foreach} -->
+        </ul>
+        {/if}
         <!-- <ul>
         <li><label class="ecjia-check"></label>
                         <input type="radio" name="test" >111111111</li>
