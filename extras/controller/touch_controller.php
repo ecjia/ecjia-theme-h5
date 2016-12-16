2851198a48cf5821e042900d61dffcc5cd1dc594
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-16 17:25:26 +0800

    修改页面跳转判断

--- extras/controller/touch_controller.php
+++ extras/controller/touch_controller.php
@@ -82,6 +82,12 @@ class touch_controller {
         ecjia_front::$controller->assign('searchs', user_function::get_search($store_id));
         ecjia_front::$controller->assign('searchs_count', count(user_function::get_search($store_id)));
         
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone')||strpos($_SERVER['HTTP_USER_AGENT'], 'iPad')) {
+        	ecjia_front::$controller->assign('down_url', ecjia::config('mobile_iphone_download'));
+        } else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Android')) {
+        	ecjia_front::$controller->assign('down_url', ecjia::config('mobile_android_download'));
+        }
+        
         ecjia_front::$controller->assign_title();
         ecjia_front::$controller->assign_lang();
 
--- library/model_download.lbi.php
+++ library/model_download.lbi.php
@@ -6,7 +6,7 @@ Description: 这是底部下载弹出层
 defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 ?>
 <div class="ecjia-download">
-	<a class="btn_install" href="">
+	<a class="btn_install" href="{$down_url}">
 		<div class="btn_download"></div>
 	</a>
 	<a class="close_tip"></a>
