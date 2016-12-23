d21a041d4ed05d888c97e2bd0ded37efe13d137b
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-23 13:17:30 +0800

    修改推广页面

--- extras/controller/user_controller.php
+++ extras/controller/user_controller.php
@@ -30,8 +30,10 @@ class user_controller {
      * 推广页面
      */
     public static function spread() {
+    	$name = $_GET['name'];
     	$token = ecjia_touch_user::singleton()->getToken();
     	$invite_user_detail = ecjia_touch_manager::make()->api(ecjia_touch_api::INVITE_USER)->data(array('token' => $token))->run();
+		
 		if (!empty($invite_user_detail['invite_explain'])) {
 			if (strpos($invite_user_detail['invite_explain'], '；')) {
 				$invite_user_detail['invite_explain_new'] = explode('；', $invite_user_detail['invite_explain']);
@@ -44,11 +46,26 @@ class user_controller {
 				}
 			}
 		}
+		ecjia_front::$controller->assign('share_title', $name.'推荐这个使用的App给你~');
 		ecjia_front::$controller->assign_title('我的推广');
     	ecjia_front::$controller->assign('invite_user', $invite_user_detail);
+    	ecjia_front::$controller->assign('url', RC_Uri::url('user/index/wxconfig'));
     	ecjia_front::$controller->display('spread.dwt');
     }
-
+	
+    public static function wxconfig() {
+    	$url = $_POST['url'];
+    	RC_Loader::load_app_class('platform_account', 'platform', false);
+    	RC_Loader::load_app_class('wechat_method', 'wechat', false);
+    	 
+    	$uuid = platform_account::getCurrentUUID('wechat');
+    	$wechat = wechat_method::wechat_instance($uuid);
+    	
+    	$config = $wechat->wxconfig($url);
+    	$config['image'] = ecjia::config('wap_logo') != '' ? RC_Upload::upload_url(ecjia::config('wap_logo')) : '';
+    	
+    	return ecjia_front::$controller->showmessage('', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS, array('data' => $config));
+    }
     /**
      * 邮件验证
      */
--- spread.dwt.php
+++ spread.dwt.php
@@ -13,52 +13,62 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <script type="text/javascript">
 ecjia.touch.spread.init();
 
-// wx.config({
-//     debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-//     appId: '', // 必填，公众号的唯一标识
-//     timestamp: '', // 必填，生成签名的时间戳
-//     nonceStr: '', // 必填，生成签名的随机串
-//     signature: '',// 必填，签名，见附录1【必填：通过提供接口获取】
-//     jsApiList: [
-//         'checkJsApi',
-//         'onMenuShareTimeline',
-//         'onMenuShareAppMessage'
-// 	]
-//      // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-// });
-// wx.ready(function () {
-// 	var newHdUrl = ''+ '&fxOpenid=';
-// 	    wx.onMenuShareTimeline({
-//         title: '', // 分享标题【必填】
-//         link: newHdUrl, // 分享链接【必填】
-//         imgUrl: 'http://www.jeewx.com/P3-Web/content/bargain/images/top_01.png', // 分享图标【必填】
-//         success: function () { 
-//             // 用户确认分享后执行的回调函数
-//         },
-//         cancel: function () { 
-//             // 用户取消分享后执行的回调函数
-//         }
-//     });
-    
-  
-//     wx.onMenuShareAppMessage({
-//         title: '', // 分享标题【必填】
-//         desc: '东海音乐节起源于2011年，经过4年的持续举办，我们已经成为长三角地区最吸引人的户外音乐节之一。我们致力于将最好的音乐现场良性的融入城市生态，与观众共同创造一年一度的海洋音乐狂欢。', // 分享描述【必填】
-//         link: newHdUrl, // 分享链接【必填】
-//         imgUrl: 'http://www.jeewx.com/P3-Web/content/bargain/images/top_01.png', // 分享图标【必填】
-//         type: 'link', // 分享类型,music、video或link，不填默认为link【必填】
-//         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-//         success: function () { 
-//             // 用户确认分享后执行的回调函数
-// 			alert('已分享');
-//         },
-//         cancel: function () { 
-//             // 用户取消分享后执行的回调函数
-// 			 alert('已取消');
-//         }
-//     });
-// });
-
+/***用户打开页面的时候就加载**/
+$(document).ready(function(){
+	initPage();
+});
+function initPage() {
+	var url = "{$url}";
+	var info = {
+		'url' : window.location.href,
+	};
+		
+	$.post(url, info, function(response, status){
+		var data = response.data;
+		wx.config({
+			debug: true,
+			appId: data.appId,
+			timestamp: data.timestamp,
+			nonceStr: data.nonceStr,
+			signature: data.signature,
+			jsApiList: [
+				'checkJsApi',
+				'onMenuShareTimeline',
+				'hideOptionMenu',
+			]
+		});
+		wx.ready(function () {
+			wx.onMenuShareTimeline({
+		        title: "{$share_title}", // 分享标题【必填】
+		        link: "{$invite_user.invite_url}", // 分享链接【必填】
+		        imgUrl: data.image, // 分享图标【必填】
+		        success: function () { 
+		            // 用户确认分享后执行的回调函数
+		        },
+		        cancel: function () { 
+		            // 用户取消分享后执行的回调函数
+		        }
+		    });
+	
+		    wx.onMenuShareAppMessage({
+		        title: "{$share_title}", // 分享标题【必填】
+		        desc: "{$invite_user.invite_template}", // 分享描述【必填】
+		        link: "{$invite_user.invite_url}", // 分享链接【必填】
+		        imgUrl: data.image, // 分享图标【必填】
+		        type: 'link', // 分享类型,music、video或link，不填默认为link【必填】
+		        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+		        success: function () { 
+		            // 用户确认分享后执行的回调函数
+					alert('已分享');
+		        },
+		        cancel: function () { 
+		            // 用户取消分享后执行的回调函数
+					alert('已取消');
+		        }
+		    });
+		});	
+	});
+};
 </script>
 <!-- {/block} -->
 
