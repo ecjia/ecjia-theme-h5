748003c0b07bd3c99be1e810d66f5782b67f29f6
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-28 09:19:30 +0800

    删除多余弹窗插件调用

--- user_order_list.dwt.php
+++ user_order_list.dwt.php
@@ -43,106 +43,49 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
       <li class="confirm-title-ok-cancel">Confirm with text, title, Ok</li>
     </ul>
 </div> -->
-<script type="text/javascript">
-        var myApp = new Framework7();
-        // var $ = Dom7;
-        $('.alert-text').on('click', function () {
-            myApp.alert('Here goes alert text');
-        });
-
-        $('.alert-text-title').on('click', function () {
-            myApp.alert('Here goes alert text', 'Custom Title!');
-        });
-
-        $('.alert-text-title-callback').on('click', function () {
-            myApp.alert('Here goes alert text', 'Custom Title!', function () {
-                myApp.alert('Button clicked!')
-            });
-        });
-
-        $('.alert-text-callback').on('click', function () {
-            myApp.alert('Here goes alert text', function () {
-                myApp.alert('Button clicked!')
-            });
-        });
-
-        $('.confirm-ok').on('click', function () {
-            myApp.confirm('Are you sure?', function () {
-                myApp.alert('You clicked Ok button');
-            });
-        });
-
-        $('.confirm-ok-cancel').on('click', function () {
-            myApp.confirm('Are you sure?',
-              function () {
-                myApp.alert('You clicked Ok button');
-              },
-              function () {
-                myApp.alert('You clicked Cancel button');
-              }
-            );
-        });
-        $('.confirm-title-ok').on('click', function () {
-            myApp.confirm('Are you sure?', 'Custom Title', function () {
-                myApp.alert('You clicked Ok button');
-            });
-        });
-        $('.confirm-title-ok-cancel').on('click', function () {
-            myApp.confirm('Are you sure?', 'Custom Title',
-              function () {
-                myApp.alert('You clicked Ok button');
-              },
-              function () {
-                myApp.alert('You clicked Cancel button');
-              }
-            );
-        });
-</script>
 <!-- {/block} -->
 
 <!-- {block name="ajaxinfo"} -->
 <!-- {foreach from=$order_list item=list} -->
-		<li class="ecjia-order-item ecjia-checkout ecjia-margin-t">
-			<div class="order-hd">
-				<a class="ecjiaf-fl" href='{url path="merchant/index/init" args="store_id={$list.seller_id}"}'>
-					<i class="iconfont icon-shop"></i>{$list.seller_name} <i class="iconfont icon-jiantou-right"></i>
-				</a>
-				<a class="ecjiaf-fr" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}"}'><span class="ecjia-color-green">{$list.label_order_status}</span></a>
-			</div>
-			<div class="flow-goods-list">
-				<a class="ecjiaf-db" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}&type=detail"}'>
-					<ul class="{if count($list.goods_list) > 1}goods-list{else}goods-item{/if}"><!-- goods-list 多个商品隐藏商品名称,goods-item -->
-						<!-- {foreach from=$list.goods_list item=goods name=goods} -->
-						<!-- {if $smarty.foreach.goods.iteration gt 3} -->
-						<!-- 判断不能大于4个 -->
-						<li class="goods-img-more">
-							<i class="icon iconfont">&#xe62e;</i>
-							<p class="ecjiaf-ib">共{$list.goods_number}件</p>
-						</li>
-						<!-- {break} -->
-						<!-- {/if} -->
-						<li class="goods-img ecjiaf-fl ecjia-margin-r ecjia-icon">
-							<img class="ecjiaf-fl" src="{$goods.img.thumb}" alt="{$goods.name}" title="{$goods.name}" />
-							{if $goods.goods_number gt 1}<span class="ecjia-icon-num top">{$goods.goods_number}</span>{/if}
-							<span class="ecjiaf-fl goods-name ecjia-truncate2">{$goods.name}</span>
-						</li>
-						<!-- {/foreach} -->
-					</ul>
-				</a>
-			</div>
-			<div class="order-ft">
-				<span><a href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}&type=detail"}'>订单金额：<span>{$list.formated_total_fee}</span></a></span>
-				<span class="two-btn ecjiaf-fr">
-				{if $list.order_status_code eq 'await_pay'} <a class="btn btn-hollow" href='{url path="pay/index/init" args="order_id={$list.order_id}&from=list"}'>去支付</a>
-				<!-- if $list.order_status_code eq 'finished' || $list.order_status_code eq 'canceled' -->
-				{else} <a class="btn btn-hollow" href='{url path="user/user_order/buy_again" args="order_id={$list.order_id}&from=list"}'>再次购买</a>
-				{/if}
-				{if $list.shipping_status eq '1'} <a class="btn btn-hollow" href='{url path="user/user_order/affirm_received" args="order_id={$list.order_id}&from=list"}'>确认收货</a>{/if}
-				</span>
-			</div>
-		</li>
-		<!-- {foreachelse} -->
-    	
-		<!-- {/foreach} -->
-		
+<li class="ecjia-order-item ecjia-checkout ecjia-margin-t">
+	<div class="order-hd">
+		<a class="ecjiaf-fl" href='{url path="merchant/index/init" args="store_id={$list.seller_id}"}'>
+			<i class="iconfont icon-shop"></i>{$list.seller_name} <i class="iconfont icon-jiantou-right"></i>
+		</a>
+		<a class="ecjiaf-fr" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}"}'><span class="ecjia-color-green">{$list.label_order_status}</span></a>
+	</div>
+	<div class="flow-goods-list">
+		<a class="ecjiaf-db" href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}&type=detail"}'>
+			<ul class="{if count($list.goods_list) > 1}goods-list{else}goods-item{/if}"><!-- goods-list 多个商品隐藏商品名称,goods-item -->
+				<!-- {foreach from=$list.goods_list item=goods name=goods} -->
+				<!-- {if $smarty.foreach.goods.iteration gt 3} -->
+				<!-- 判断不能大于4个 -->
+				<li class="goods-img-more">
+					<i class="icon iconfont">&#xe62e;</i>
+					<p class="ecjiaf-ib">共{$list.goods_number}件</p>
+				</li>
+				<!-- {break} -->
+				<!-- {/if} -->
+				<li class="goods-img ecjiaf-fl ecjia-margin-r ecjia-icon">
+					<img class="ecjiaf-fl" src="{$goods.img.thumb}" alt="{$goods.name}" title="{$goods.name}" />
+					{if $goods.goods_number gt 1}<span class="ecjia-icon-num top">{$goods.goods_number}</span>{/if}
+					<span class="ecjiaf-fl goods-name ecjia-truncate2">{$goods.name}</span>
+				</li>
+				<!-- {/foreach} -->
+			</ul>
+		</a>
+	</div>
+	<div class="order-ft">
+		<span><a href='{url path="user/user_order/order_detail" args="order_id={$list.order_id}&type=detail"}'>订单金额：<span>{$list.formated_total_fee}</span></a></span>
+		<span class="two-btn ecjiaf-fr">
+		{if $list.order_status_code eq 'await_pay'} <a class="btn btn-hollow" href='{url path="pay/index/init" args="order_id={$list.order_id}&from=list"}'>去支付</a>
+		<!-- if $list.order_status_code eq 'finished' || $list.order_status_code eq 'canceled' -->
+		{else} <a class="btn btn-hollow" href='{url path="user/user_order/buy_again" args="order_id={$list.order_id}&from=list"}'>再次购买</a>
+		{/if}
+		{if $list.shipping_status eq '1'} <a class="btn btn-hollow" href='{url path="user/user_order/affirm_received" args="order_id={$list.order_id}&from=list"}'>确认收货</a>{/if}
+		</span>
+	</div>
+</li>
+<!-- {foreachelse} -->
+<!-- {/foreach} -->
 <!-- {/block} -->
