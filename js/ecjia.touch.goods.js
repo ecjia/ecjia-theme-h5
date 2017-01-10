36446b10ef4f577a3bc953563e2749e82e4e57f9
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-10 11:52:11 +0800

    优化加载店铺商品

--- js/ecjia.touch.goods.js
+++ js/ecjia.touch.goods.js
@@ -375,9 +375,9 @@
         	} else {
             	//禁用滚动条
             	$('body').css('overflow-y', 'hidden').on('touchmove',function(event){event.preventDefault;}, false);
-//        		$('.minicart-content').on('touchmove', function(e) {
-//        			e.stopPropagation();
-//        		});
+        		$('.minicart-content').on('touchmove', function(e) {
+        			e.stopPropagation();
+        		});
             	$('.store-add-cart').children('.a4x').removeClass('show');
             	$('.minicart-content').children('.a4x').addClass('show').addClass('light').removeClass('disabled');
             	$('.store-add-cart').children('.minicart-content').css('transform', 'translateY(-100%)');
@@ -456,16 +456,25 @@
         		var type = $this.attr('data-type') == undefined ? category_id : $this.attr('data-type');
         		$('[data-toggle="asynclist"]').attr('class', 'store_goods_' + type);
         		
+        		var store_id = $('input[name="store_id"]').val();
         		var info = {'action_type' : type};
         		$('.wd').find('[data-toggle="asynclist"]').attr('data-type', type);
 
         		if (bool == true) {
         			$('.wd').find('[data-toggle="asynclist"]').html('');
             		$('.load-list').remove();
-            		ecjia.touch.asynclist();
+            		$loader = $('<a class="load-list" href="javascript:;"><div class="loaders"><div class="loader"><div class="loader-inner ball-pulse"><div></div><div></div><div></div></div></div></div></a>');
+            		$('.store_goods_' + type).after($loader);
+    				
             		$.get(url, info, function(data) {
+            			$loader.remove();
+            			$('.store_goods_' + type).append(data.list);
             			var info = data.name + '(' + data.num + ')';
             			$('.a20').html(info);
+            			if (data.is_last == null) {
+            				$('.store_goods_' + type).attr('data-page', 2);
+            				ecjia.touch.asynclist();
+            			}
             		});
         		}
         	});
