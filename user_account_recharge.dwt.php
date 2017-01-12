974ac0822fa3d6b0f9e5397d65b228b81bdcede1
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-12 18:28:08 +0800

    修改H5充值页面样式

--- user_account_recharge.dwt.php
+++ user_account_recharge.dwt.php
@@ -8,6 +8,12 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 ?>
 <!-- {extends file="ecjia-touch.dwt.php"} -->
 
+<!-- {block name="footer"} -->
+<script type="text/javascript" >
+ecjia.touch.init();
+</script>
+<!-- {/block} -->
+
 <!-- {block name="main-content"} -->
 <form class="ecjia-account ecjia-form user-profile-form" action="{url path='user/user_account/recharge_account'}" method="post">
     <div class="ecjia-form ecjia-account ecjia-flow-done ecjia-pay">
@@ -20,20 +26,20 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
     	</div>
     	<ul class="ecjia-list ecjia-margin-t">
             <li class="list-font-size">
-                                    其它支付方式 <span class="ecjiaf-fr"><i class="iconfont icon-jiantou-bottom"></i></span>
+                                    其它支付方式
             </li>
         </ul>
         <ul class="ecjia-list list-short payment-list">
             <li class="ecjia-account-padding-input">
-                <span class="icon-name {$pay_alipay.pay_code}" data-code="{$pay_alipay.pay_code}">
-                <input type="radio" id="{$pay_alipay.pay_code}" name="payment" value="{$pay_alipay.pay_id}" checked="true">
-                <label for="{$pay_alipay.pay_code}" class="ecjiaf-fr one-select" value="10"></label>
+                <span class="icon-name {$pay_alipay.pay_code} fmtmargin" data-code="{$pay_alipay.pay_code}">
+                <label for="{$pay_alipay.pay_code}" class="ecjiaf-fr ecjia-check" value="10"> 
+                <input type="radio" id="{$pay_alipay.pay_code}" name="payment" value="{$pay_alipay.pay_id}" checked="true"></label>
                 {$pay_alipay.pay_name}</a></span>
             </li>
             <li class="ecjia-account-padding-input">
-                <span class="icon-name {$pay_wxpay.pay_code}" data-code="{$pay_wxpay.pay_code}">
-                <input type="radio" id="{$pay_wxpay.pay_code}" name="payment" value="{$pay_wxpay.pay_id}">
-                <label for="{$pay_wxpay.pay_code}" class="ecjiaf-fr one-select" value="10"></label>
+                <span class="icon-name {$pay_wxpay.pay_code} fmtmargin" data-code="{$pay_wxpay.pay_code}">
+                <label for="{$pay_wxpay.pay_code}" class="ecjiaf-fr ecjia-check" value="10">      
+                <input type="radio" id="{$pay_wxpay.pay_code}" name="payment" value="{$pay_wxpay.pay_id}"></label>
                 {$pay_wxpay.pay_name}</a></span>
             </li>
         </ul>
