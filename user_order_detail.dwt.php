afa2d78afc82e3c5d328773fb024b2c5edb7787d
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-19 11:02:47 +0800

    配送员订单状态增加打电话

--- user_order_detail.dwt.php
+++ user_order_detail.dwt.php
@@ -23,7 +23,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 		<div class='order-log-item {$info.status} {if count($order.order_status_log) lt 2} item-only{/if}'>
 			<div class="order-log">
 				<span>{$info.order_status}</span><span class="ecjiaf-fr order-time">{$info.time}</span>
-				<p>{$info.message}</p>
+				<p>{$info.message}</p>{if $info.status eq 'express_user_pickup' && $order.express_mobile}<a class="tel" href="tel://{$order.express_mobile}"></a>{/if}
 			</div>
 		</div>
 		<!-- {/foreach} -->
