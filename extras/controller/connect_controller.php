2665188d1fae09e5006f1002ab72d62c3794deeb
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-02-08 14:08:13 +0800

    微信登录调试用户信息删除

--- extras/controller/connect_controller.php
+++ extras/controller/connect_controller.php
@@ -103,60 +103,6 @@ class connect_controller {
         return ecjia_front::$controller->fetch ('user_bind_login.dwt.php');
     }
     
-    /**
-     * 授权用户绑定已有账号
-     * 
-     */
-//     public static function user_bind($get) {
-//         ecjia_front::$controller->assign('page_title', "授权登录账号绑定-".ecjia::config('shop_name'));
-//         ecjia_front::$controller->assign('get', $get);
-//         ecjia_front::$controller->assign('shop_address', "上海市普陀区中山北路3553号伸大厦3层");
-//         //copyright
-//         return file_get_contents (RC_Theme::get_template_directory().'/user_bind.dwt.php');
-//     }
-
-    public static function dump_user_info(){
-        $code = $_GET['code'];
-        $appsecret = 'f5f9db6dfb1565d61528c749d6fdb4f3';
-        $appid = 'wx7daa4b18c79dcd49';
-        $referer = empty($_GET['referer'])? 'init' : $_GET['referer'];
-        $id = empty($_GET['id'])? '' : intval($_GET['id']);
-        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$appsecret.'&code='.$code.'&grant_type=authorization_code';
-        $token_temp = file_get_contents($token_url);
-        $token = json_decode($token_temp);
-        if (isset($token->errcode)) {
-            echo '<h1>错误：</h1>'.$token->errcode;
-            echo '<br/><h2>错误信息：</h2>'.$token->errmsg;
-            exit;
-        }
-    
-        $access_token_url = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid='.$appid.'&grant_type=refresh_token&refresh_token='.$token->refresh_token;
-        $access_token_url_temp = file_get_contents($access_token_url);
-        $access_token = json_decode($access_token_url_temp);
-        if (isset($access_token->errcode)) {
-            echo '<h1>错误：</h1>'.$access_token->errcode;
-            echo '<br/><h2>错误信息：</h2>'.$access_token->errmsg;
-            exit;
-        }
-    
-        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token->access_token.'&openid='.$access_token->openid.'&lang=zh_CN';
-        $user_info_temp = file_get_contents($user_info_url);
-        $user_info = json_decode($user_info_temp);
-        if (isset($user_info->errcode)) {
-            echo '<h1>错误：</h1>'.$user_info->errcode;
-            echo '<br/><h2>错误信息：</h2>'.$user_info->errmsg;
-            exit;
-        }
-        $arr = array();
-        if(!empty($id)){
-            $arr = array('id' => $id);
-        }
-        _dump($user_info,1);
-        $url = RC_Uri::url('event/index/'.$referer, $arr);
-        $_SESSION['openid'] = $user_info->openid;
-        ecjia_front::$controller->redirect($url);
-    
-    }
     
     /* 第三方登陆 */
     public static function bind_login() {
