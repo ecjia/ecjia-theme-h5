8cd547bd75501d278e18045b6aef25b254044f52
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-02-23 09:11:50 +0800

    调整店铺入驻短线样式

--- franchisee_store_msg.dwt.php
+++ franchisee_store_msg.dwt.php
@@ -16,37 +16,38 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 
 <!-- {block name="main-content"} -->
 <form class="ecjia-address-list" name="theForm" action="{$form_action}" method="post">
-	<div class="form-group form-group-text">
+	<div class="form-group form-group-text franchisee">
 		<label class="input">
 			<span class="ecjiaf-fl"><img src="{$theme_url}/images/user_center/f_store.png" width="30" height="30"></span>
-			<input style="padding-left: 3.5em;" name="f_name" placeholder="{t}请输入店铺名称10字以内{/t}" type="text"  value=""  />
+			<input name="f_name" placeholder="{t}请输入店铺名称10字以内{/t}" type="text"  value=""  />
 		</label>
 	</div>
 	
-	<div class="form-group form-group-text">
+	<div class="form-group form-group-text franchisee">
 		<label class="input">
 			<span class="ecjiaf-fl"><img src="{$theme_url}/images/user_center/f_category.png" width="30" height="30"></span>
+			<i class="iconfont  icon-jiantou-right"></i>
 			<input class="ecjia-franchisee-category" style="padding-left: 3.5em;" name="f_email" placeholder="{t}请选择店铺分类{/t}" type="email"  value=""  />
 		</label>
-	</div>
-	
-	<div class="form-group form-group-text">
 		<label class="input">
 			<span class="ecjiaf-fl"><img src="{$theme_url}/images/user_center/f_type.png" width="30" height="30"></span>
+			<i class="iconfont  icon-jiantou-right"></i>
 			<input class="ecjia-franchisee-type" style="padding-left: 3.5em;" name="f_mobile" placeholder="{t}请选择入驻类型 {/t}" type="tel" value=""  />
 		</label>
 	</div>
 	
-	<div class="form-group form-group-text">
+	<div class="form-group form-group-text franchisee">
 		<label class="input">
-			<span class="ecjiaf-fl"><img src="{$theme_url}/images/user_center/f_location.png" width="30" height="30"></span>
-			<i class="iconfont  icon-jiantou-right"></i>
-			<input style="padding-left: 3.5em;" name="f_city" placeholder="{t}选择店铺所在地{/t}" type="text" value=""   />
-			<input style="padding-left: 3.5em;" name="f_address" placeholder="{t}输入详细地址{/t}" type="text" value=""   />
+    		<span class="ecjiaf-fl"><img src="{$theme_url}/images/user_center/f_location.png" width="30" height="30"></span>
+    		<i class="iconfont  icon-jiantou-right"></i>
+    		<input name="f_city" placeholder="{t}选择店铺所在地{/t}" type="text" value=""   />
+		</label>
+		<label class="input">
+    	   <input name="f_address" placeholder="{t}输入详细地址{/t}" type="text" value=""   />
 		</label>
 	</div>
 	
-	<p class="coordinate" data-url="{url path='franchisee/index/get_loaction'}">获取精准坐标</p>
+	<p class="coordinate" data-url="{url path='franchisee/index/location'}">获取精准坐标</p>
 	
 	<div class="ecjia-margin-t ecjia-margin-b">
 	    <input name="temp_key" type="hidden" value="{$temp_key}" />
