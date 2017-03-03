dacdff26cab75a5e9f11fbefcaeaca3007490acb
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-03 17:56:56 +0800

    提交h5模板及样式修改

--- library/merchant_goods.lbi.php
+++ library/merchant_goods.lbi.php
@@ -13,7 +13,6 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- {/block} -->
 
 <!-- {block name="ajaxinfo"} -->
-
 <!-- {foreach from=$goods_list item=goods} -->
 <li>
 	<a class="linksGoods w" href="{RC_Uri::url('goods/index/show')}&goods_id={$goods.id}">
@@ -23,11 +22,15 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 			<dd><label>{$goods.shop_price}</label></dd>
 		</dl>
 	</a>
+	{if $goods.goods.attr}
+	<div class="goods_attr"><span class="choose_attr">选规格</span></div>
+	{else}
 	<div class="box" id="goods_{$goods.id}">
     	<span class="reduce {if $goods.num}show{else}hide{/if}" data-toggle="remove-to-cart" rec_id="{$goods.rec_id}">减</span>
     	<label class="{if $goods.num}show{else}hide{/if}">{$goods.num}</label>
 		<span class="add" data-toggle="add-to-cart" rec_id="{$goods.rec_id}" goods_id="{$goods.id}">加</span>
 	</div>
+	{/if}
 </li>
 <!-- {foreachelse} -->	
 <div class="ecjia-merchant-goods ecjia-nolist">
---
+++ library/suggest_store.lbi.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Name: 首页推荐商家
+Description: 这是推荐商家
+Libraries: suggest_store
+*/
+defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
+?>
+<!-- {extends file="ecjia-touch.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">ecjia.touch.category.init();</script>
+<!-- {/block} -->
+
+<!-- {block name="ajaxinfo"} -->
+<!-- {foreach from=$data item=val} -->
+<!-- {/foreach} -->	
+<!-- {/block} -->
