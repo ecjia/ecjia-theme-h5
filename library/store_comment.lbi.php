754b7d4cdabfb2194d9b8e714d9fb1842442b41a
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-06 11:33:54 +0800

    修改评论ajax_info到单独文件

--- library/store_comment.lbi.php
+++ library/store_comment.lbi.php
@@ -40,37 +40,3 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 		</span>    
 	</div>
 </div>
-
-<!-- {block name="ajaxinfo"} -->
-<!-- {foreach from=$comment_list item=list} -->
-<div class="assess-flat">    
-	<span class="assess-wrapper">        
-		<div class="assess-top">            
-			<span class="user-portrait">                
-			<img src="{$theme_url}images/default_user.png"></span>            
-			
-			<span class="user-name">j***9</span>     
-			<span class="assess-date">2017-03-03</span>
-			<span class="comment-item-star">         
-				<span class="real-star comment-stars-width5"></span>
-			</span> 
-		</div>        
-		<div class="assess-bottom">            
-			<p class="assess-content">商品不错，很好吃，很满意的一次购物。</p>
-			<div class="img-list">
-				<img src="{$theme_url}images/default-goods-pic.png" />
-				<img src="{$theme_url}images/default-goods-pic.png" />
-				<img src="{$theme_url}images/default-goods-pic.png" />
-				<img src="{$theme_url}images/default-goods-pic.png" />
-				<img src="{$theme_url}images/default-goods-pic.png" />
-			</div>
-		</div>    
-	</span>    
-</div>
-<!-- {foreachelse} -->	
-<div class="ecjia-nolist">
-	<p><img src=""></p>
-	暂无评论
-</div>
-<!-- {/foreach} -->	
-<!-- {/block} -->
