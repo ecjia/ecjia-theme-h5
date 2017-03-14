c28ec589ce71c56a125a73bf601ee4e1122b32c9
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-14 11:32:51 +0800

    修改入驻信息修改

--- franchisee_process.dwt.php
+++ franchisee_process.dwt.php
@@ -61,7 +61,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
             <span class="fran-info-color">拒绝原因：</span>
             <span class="fran-info-color">{$info.remark}</span>
             <div class="hand-objection">
-               <a class="remove_apply btn" href="{RC_Uri::url('franchisee/index/reapply')}&mobile={$mobile}&code={$code}" title="申请修改信息">申请修改信息</a>
+               <a class="remove_apply btn" href="{RC_Uri::url('franchisee/index/second')}&mobile={$mobile}&code={$code}" title="申请修改信息">申请修改信息</a>
                <input class="btn" name="cancel" type="button" data-url="{$url}" value="撤销申请" />
             </div>
         {else}
--- js/ecjia.touch.franchisee.js
+++ js/ecjia.touch.franchisee.js
@@ -37,6 +37,30 @@
 				});
 			});
 		},
+
+		//发送验证码报错
+		error_validate :function(){
+			$('input[name="cancel"]').on('click', function(e){
+        		e.preventDefault();
+        		var url = $(this).attr('data-url');
+        		options = {
+					'status' : 'cancel',
+				}
+        		var myApp = new Framework7({
+					modalButtonCancel : '取消',
+					modalButtonOk : '确定',
+					modalTitle : '提示'
+			    });
+				myApp.confirm('您确定要撤销申请吗？', function () {
+					$.post(url, options,function(data){
+						if (data.log != '') {
+							ecjia.pjax(data.cancel_url);
+						}
+					});
+			    });
+				
+        	});
+		},
 		
 		//商家入驻流程获取验证码
 		validate_code : function () {
@@ -156,7 +180,10 @@
 				var address 		= $("input[name='f_address']").val();
 				var longitude 		= $("input[name='longitude']").val();
 				var latitude 		= $("input[name='latitude']").val();
-				
+				var mobile 			= $("input[name='mobile']").val();
+				var code 			= $("input[name='code']").val();
+				$.cookie('seller', $("input[name='seller_category']").val());
+				$.cookie('address', address);
 				var url = $("form[name='theForm']").attr('action');
 				if (seller_name == '') {
 					alert('请输入店铺名称');return false;
@@ -192,7 +219,9 @@
 					'district': district,
 					'address': address,
 					'longitude': longitude,
-					'latitude': latitude
+					'latitude': latitude,
+					'mobile' : mobile,
+					'code' : code
 				};
 				$.post(url, info, function(data){
 					if (data.state == 'error') {
@@ -212,7 +241,7 @@
 			$('input[name="seller_name"]').blur(function() {
 				$.cookie('seller_name', $('input[name="seller_name"]').val());
 			});
-			
+
 			var category_list = [];
 			var category = eval('(' + $("input[name='category']").val() + ')')['data'];
 			if(category == null){
@@ -411,13 +440,13 @@
 		coordinate : function () {
 			var longitude 	= $("input[name='longitude']").val();
 			var latitude 	= $("input[name='latitude']").val();
-			
+			var mobile 		= $("input[name='mobile']").val();
+			var code 		= $("input[name='code']").val();
 			if (longitude != '' && latitude != '') {
 				$(".coordinate").html("经度：" + longitude + "；  " + "纬度：" + latitude);
 			}
 
 			$(".coordinate").on('click', function(e) {
-				e.preventDefault();
 				var seller_name = $("input[name='seller_name']").val();
 				$.cookie('seller_name', seller_name); 
 				var f_province 	= $("input[name='f_province']").val();
@@ -427,7 +456,7 @@
 				
 				if(f_province && f_district && f_district && f_address){
 					var url = $(this).attr("data-url");
-					var url = url + '&province=' +f_province+ '&city=' +f_city+ '&district=' +f_district+ '&address=' +f_address;
+					var url = url + '&province=' +f_province+ '&city=' +f_city+ '&district=' +f_district+ '&address=' +f_address+ '&mobile=' +mobile+ '&code=' +code;
 					location.href = url;
 				}else{
 					alert('请输入详细地址');
@@ -440,7 +469,10 @@
 				e.preventDefault();
 				var longitude = $("input[name='longitude']").val();
 				var latitude = $("input[name='latitude']").val();
-				var url = $(this).attr("data-url")+ '&longitude=' +longitude+ '&latitude=' +latitude;
+				var mobile 		= $("input[name='mobile']").val();
+				var code 		= $("input[name='code']").val();
+				var url = $(this).attr("data-url")+ '&longitude=' +longitude+ '&latitude=' +latitude+ '&mobile=' +mobile+ '&code=' +code;
+//				
 				location.href = url;
 			})
 		},
