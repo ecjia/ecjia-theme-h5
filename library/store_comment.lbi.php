744f77ebe538adddeac65a0e118b07eda3817ce4
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-17 18:59:32 +0800

    修改样式

--- library/store_comment.lbi.php
+++ library/store_comment.lbi.php
@@ -28,39 +28,4 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	</dl>
 </div>
 <div class="store-comment" {if $comment_list.list} data-toggle="asynclist" data-loadimg="{$theme_url}dist/images/loader.gif" data-url="{$ajax_url}" data-type="all" data-page="2" {/if}>
-	{if $comment_list.list}
-	<!-- {foreach from=$comment_list.list item=list} -->
-	<div class="assess-flat">    
-		<div class="assess-wrapper">        
-			<div class="assess-top">            
-				<span class="user-portrait"><img src="{$theme_url}images/default_user.png"></span>           
-				<div class="user-right"> 
-					<span class="user-name">{$list.author}</span>     
-					<span class="assess-date">{$list.add_time}</span>
-				</div>
-				<p class="comment-item-star score-goods" data-val="{$list.rank}"></p> 
-			</div>        
-			<div class="assess-bottom">            
-				<p class="assess-content">{$list.content}</p>
-				<!-- {if $comment.picture} -->
-				<div class="img-list">
-					<!-- {foreach from=$list.picture item=img} -->
-					<img src="{$img}" />
-					<!-- {/foreach} -->
-				</div>
-				<!-- {/if} -->
-				<p class="goods-attr">{$list.goods_attr}</p>
-				{if $list.reply_content}
-				<div class="store-reply">商家回复：{$list.reply_content}</div>
-				{/if}
-			</div>    
-		</div>    
-	</div>
-	<!-- {/foreach} -->
-	{else}
-	<div class="ecjia-nolist">
-		<img src="{$theme_url}images/no_comment.png">
-		<p class="tags_list_font">暂无商品评论</p>
-	</div>
-	{/if}
 </div>
