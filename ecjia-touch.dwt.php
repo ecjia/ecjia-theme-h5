8d42b4adc4a3f714fb8aab242ad44ee094189bbb
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-08 10:06:44 +0800

    修改js代码格式，删除调试信息

--- ecjia-touch.dwt.php
+++ ecjia-touch.dwt.php
@@ -65,7 +65,6 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	<!-- {block name="meta"} --><!-- {/block} -->
 	<script type="text/javascript" src="{$theme_url}js/ecjia.touch.koala.js" ></script>
 	<script type="text/javascript" src="{$theme_url}js/ecjia.touch.js" ></script>
-    <script type="text/javascript" src="{$theme_url}js/ecjia.touch.history.js" ></script>
     <script type="text/javascript" src="{$theme_url}js/ecjia.touch.others.js" ></script>
     <script type="text/javascript" src="{$theme_url}js/ecjia.touch.goods.js" ></script>
     <script type="text/javascript" src="{$theme_url}js/ecjia.touch.user.js" ></script>
