a203c18188760d12705901cb11bf79653d08ab73
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-08 14:11:17 +0800

    增加ajax请求判断

--- user_edit_password.dwt.php
+++ user_edit_password.dwt.php
@@ -17,7 +17,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- {/block} -->
 
 <!-- {block name="main-content"} -->
-<form class="ecjia-form ecjia-login-edit-password-form ecjia-login-padding-top ecjia-user-no-border-b" name="formPassword" action="{url path='user/profile/edit_password'}" method="post" >
+<form class="ecjia-form ecjia-login-edit-password-form ecjia-login-padding-top ecjia-user-no-border-b" name="formPassword" action="{url path='user/profile/edit_password'}&type=ajax" method="post" >
 	<div class="ecjia-user ecjia-account ecjia-form border-t">
         <ul>
             <div class="ecjia-list ecjia-list-normal form-group right-angle ecjia-user-no-border-b">
