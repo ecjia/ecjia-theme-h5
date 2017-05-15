d96d3f6512f267c3d12ebd8f87fa1e53adccc1e8
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-15 16:35:59 +0800

    订单添加搜索栏样式

--- user_order_list.dwt.php
+++ user_order_list.dwt.php
@@ -14,10 +14,23 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	{foreach from=$lang.merge_order_js item=item key=key}
 		var {$key} = "{$item}";
 	{/foreach}
+	ecjia.touch.enter_search();
 </script>
 <!-- {/block} -->
 <!-- {block name="main-content"} -->
 <!-- #EndLibraryItem -->
+
+<header class="ecjia-order-search">
+    <div class="ecjia-header">
+    	<div class="ecjia-search-header ecjia-search">
+    		<form class="ecjia-form" action="{url path='user/order/order_list'}{if $store_id neq 0}&store_id={$store_id}{/if}">
+    			<input id="keywordBox" name="keywords" type="search" placeholder="商品名称/订单号" {if $keywords}value={$keywords}{/if}>
+    			<i class="iconfont icon-search btn-search"></i>
+    		</form>
+    	</div>
+	</div>
+</header>
+
 <div class="ecjia-order-list ">
     {if $order_list}
 	<ul class="ecjia-margin-b" id="J_ItemList" data-toggle="asynclist" data-loadimg="{$theme_url}dist/images/loader.gif" data-url="{url path='order/async_order_list'}" data-size="10" data-page="1">
@@ -33,6 +46,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 <!-- {/block} -->
 
 <!-- {block name="ajaxinfo"} -->
+<div class="ecjia-order-item-list">
 <!-- {foreach from=$order_list item=list} -->
 <li class="ecjia-order-item ecjia-checkout ecjia-margin-t">
 	<div class="order-hd">
@@ -87,5 +101,6 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 </li>
 <!-- {foreachelse} -->
 <!-- {/foreach} -->
+</div>
 <!-- {/block} -->
 {/nocache}
