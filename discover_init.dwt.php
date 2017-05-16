b3c796c3da60adec4a5296e204e279d961396ce0
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-16 14:00:10 +0800

    增加百宝箱图标

--- discover_init.dwt.php
+++ discover_init.dwt.php
@@ -26,13 +26,11 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 	<div class="ecjia-discover-icon">
 		<div class="swiper-container" id="swiper-discover-icon">
 			<div class="swiper-wrapper">
-				<div class="swiper-slide"><a href="{RC_Uri::url('user/index/spread')}"><img src="{$theme_url}images/user_center/expand.png" /><span>推广</span></a></div>
-				<div class="swiper-slide"><a href="{RC_Uri::url('user/account/init')}"><img src="{$theme_url}images/user_center/50x50_1.png" /><span>钱包</span></a></div>
-				<div class="swiper-slide"><a href="{RC_Uri::url('goods/index/new')}"><img src="{$theme_url}images/icon/new.png" /><span>新品推荐</span></a></div>
-				<div class="swiper-slide"><a href="{RC_Uri::url('goods/index/promotion')}"><img src="{$theme_url}images/icon/promotion.png" /><span>促销商品</span></a></div>
-				<div class="swiper-slide"><a href="{RC_Uri::url('merchant/index/position')}&shop_address={$smarty.cookies.location_name}"><img src="{$theme_url}images/user_center/50x50_6.png" /><span>地图</span></a></div>
+				<div class="swiper-slide"><a href="{RC_Uri::url('user/index/spread')}"><img src="{$theme_url}images/discover/200_1.png" /><span>百宝箱</span></a></div>
 				<div class="swiper-slide"><a href="{$signup_reward_url}"><img src="{$theme_url}images/user_center/newbie_gift75_1.png" /><span>新人有礼</span></a></div>
-				<div class="swiper-slide"><a href="{url path='article/help/init'}"><img src="{$theme_url}images/user_center/help_center.png" /><span>帮助中心</span></a></div>
+				<div class="swiper-slide"><a href="{RC_Uri::url('user/index/spread')}"><img src="{$theme_url}images/user_center/expand.png" /><span>推广</span></a></div>
+				<div class="swiper-slide"><a href="{RC_Uri::url('goods/index/new')}"><img src="{$theme_url}images/discover/200_3.png" /><span>新品推荐</span></a></div>
+				<div class="swiper-slide"><a href="{RC_Uri::url('goods/index/promotion')}"><img src="{$theme_url}images/discover/200_4.png" /><span>促销商品</span></a></div>
 			</div>
 		</div>
 	</div>
