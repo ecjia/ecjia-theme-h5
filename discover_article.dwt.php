cefa24f202fdabe5b1acff7114bf07563f11c2bd
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-01 14:48:50 +0800

    修改样式

--- discover_article.dwt.php
+++ discover_article.dwt.php
@@ -83,14 +83,6 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 								<!--{else}-->
 								<span class="goods-price">{$goods.shop_price}</span>
 								<!--{/if}-->
-								{if $goods.specification}
-									<div class="goods_attr goods_spec_{$goods.goods_id}" goods_id="{$goods_info.id}">
-										<span class="choose_attr spec_goods" rec_id="{$goods.rec_id}" goods_id="{$goods.goods_id}" data-num="{$goods.num}" data-spec="{$goods.default_spec}" data-url="{RC_Uri::url('cart/index/check_spec')}" data-store="{$store_id}">选规格</span>
-										{if $goods.num}<i class="attr-number">{$goods.num}</i>{/if}
-									</div>
-								{else}
-									<span class="goods-price-plus may_like_{$goods.goods_id}" data-toggle="add-to-cart" rec_id="{$goods.rec_id}" goods_id="{$goods.goods_id}" data-num="{$goods.num}"></span>
-								{/if}
 							</div>
 						</li>
 						<!--{/if}-->
