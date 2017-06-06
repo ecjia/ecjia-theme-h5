9f402b093040603da549fe24638e469970bc4b0d
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-06 12:00:09 +0800

    修改js

--- extras/controller/article_controller.php
+++ extras/controller/article_controller.php
@@ -163,8 +163,8 @@ class article_controller {
     	$article_id = !empty($_GET['article_id']) ? intval($_GET['article_id']) : 0;
     	$token = ecjia_touch_user::singleton()->getToken();
     	$article_param = array(
-    			'token' 		=> $token,
-    			'article_id'	=> $article_id
+    		'token' 		=> $token,
+    		'article_id'	=> $article_id
     	);
     	$article_info = ecjia_touch_manager::make()->api(ecjia_touch_api::ARTICLE_DETAIL)->data($article_param)->run();
     	if (!is_ecjia_error($article_info) && !empty($article_info)) {
