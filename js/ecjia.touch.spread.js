ed640be4567759d76512bbd57465a73cf8a9fadb
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-12 10:49:24 +0800

    增加我要推广弹窗提示

--- js/ecjia.touch.spread.js
+++ js/ecjia.touch.spread.js
@@ -10,8 +10,8 @@
 			ecjia.touch.spread.share_spread();
 		},
 		spread: function() {
-			$('.would-spread').off('click').on('click', function() {
-				
+			$('.would-spread').off('click').on('click', function(e) {
+				e.preventDefault();
 				var ua = navigator.userAgent.toLowerCase();
 				if (ua.match(/MicroMessenger/i) == "micromessenger") {
 	        		$('.ecjia-spread-share').removeClass('hide').css('top', $('body').scrollTop() + 'px');
@@ -22,6 +22,8 @@
 	            		$('body').css('overflow-y', 'auto').off("touchmove");//启用滚动条
 	            	})
 	            	ecjia.touch.spread.share_spread();
+				} else {
+					alert('请在微信客户端打开链接');
 				}
         	});
         },  
