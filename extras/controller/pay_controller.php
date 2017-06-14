f931da6b9fd67f3c126b802b68635179b9a576d9
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-14 17:50:35 +0800

    修改免费商品直接余额支付

--- extras/controller/pay_controller.php
+++ extras/controller/pay_controller.php
@@ -101,7 +101,12 @@ class pay_controller {
         	}
         	 
         	$order = $rs_pay['payment'];
-        	 
+        	
+        	//免费商品直接余额支付
+        	if ($order['order_amount'] == 0) {
+        		$order['pay_cod'] = 'pay_balance';
+        	}
+        	
         	$need_other_payment = 0;
         	if ($order ['pay_code'] == 'pay_balance') {
         		if ($rs_pay['payment']['error_message']) {
