821cec0e5b76632db76f1c13f69b6a28c40cee95
靳翔 <huangyuyuan@ecmoban.com1huangyuyuan@ecmoban.com0huangyuyuan@ecmoban.com1huangyuyuan@ecmoban.com5huangyuyuan@ecmoban.com3huangyuyuan@ecmoban.com6huangyuyuan@ecmoban.com4huangyuyuan@ecmoban.com8huangyuyuan@ecmoban.com5huangyuyuan@ecmoban.com7huangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-25 19:15:33 +0800

    修改充值页面效果

--- js/ecjia.touch.user_account.js
+++ js/ecjia.touch.user_account.js
@@ -6,6 +6,7 @@
 	ecjia.touch.user_account = {
 		init: function() {
 			ecjia.touch.user_account.wxpay_user_account();
+			ecjia.touch.user_account.btnflash();
 		},
 		wxpay_user_account: function() {
 			$('.wxpay-btn').on('click', function(e) {
@@ -24,7 +25,37 @@
 					}
 				});
 			});
-		}
+		},
+		
+		btnflash : function() {
+			$('.alipay-btn').on('click', function(e) {
+				e.preventDefault();
+				$('body').append('<div class="la-ball-atom"><div></div><div></div><div></div><div></div></div>');
+				
+				var amount = $('input[name="amount"]').val();
+				if (amount == '') {
+					$('.la-ball-atom').remove();
+					alert("金额不能为空");
+					return false;
+				}
+
+				$(this).val("支付请求中，请稍后");
+				$(this).attr("disabled", true); 
+				$(this).css("border", 0);
+				$(this).css("background", "#ddd");
+				$(this).css("color", "black");
+				
+				var url = $("form[name='useraccountForm']").attr('action');
+				$("form[name='useraccountForm']").ajaxSubmit({
+					type: 'post',
+					url: url,
+					dataType: "json",
+					success: function(data) {
+						ecjia.touch.showmessage(data);
+					}
+				});
+			});
+		},
 	};
 })(ecjia, jQuery);
 
