3fa87c14a9a810ee527a7fa782170a5d0e0bbd48
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-25 16:34:44 +0800

    余额充值时，支持老的支持方式接口

--- extras/controller/user_account_controller.php
+++ extras/controller/user_account_controller.php
@@ -204,7 +204,7 @@ class user_account_controller {
 //     		    } else {
     		        $pay = ecjia_touch_manager::make()->api(ecjia_touch_api::USER_ACCOUNT_PAY)->data(array('account_id' => $data_account_id, 'payment_id' => $data_payment_id, 'wxpay_open_id' => $_SESSION['wxpay_open_id']))->run();
     		        if (! is_ecjia_error($pay)) {
-    		            $pay_online = array_get($pay, 'payment.private_data.pay_online');
+    		            $pay_online = array_get($pay, 'payment.private_data.pay_online', array_get($pay, 'payment.pay_online'));
     		            RC_Logger::getLogger('pay')->info('user_account_controller::recharge_account');
     		            RC_Logger::getLogger('pay')->info($pay);
     		            RC_Logger::getLogger('pay')->info($pay_online);
