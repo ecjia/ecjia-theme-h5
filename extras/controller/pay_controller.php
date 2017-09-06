4d008803939d9e1665c3f82abd6db076328c38ca
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-06 10:09:21 +0800

    更新支付通知页面

--- extras/controller/pay_controller.php
+++ extras/controller/pay_controller.php
@@ -203,7 +203,14 @@ class pay_controller {
     		$msg = '支付成功';
     		ecjia_front::$controller->assign('msg', $msg);
     		$order_type = isset($_GET['order_type']) ? trim($_GET['order_type']) : '';
+    		$url['index'] = RC_Cookie::get('pay_response_index');
+    		$url['order'] = RC_Cookie::get('pay_response_order');
     		
+    		$url = array(
+    		    'index' => RC_Cookie::get('pay_response_index') ? RC_Cookie::get('pay_response_index') : str_replace('notify/', '', RC_Uri::url('touch/index/init')),
+    		    'order' => RC_Cookie::get('pay_response_order') ? RC_Cookie::get('pay_response_order') : str_replace('notify/', '', RC_Uri::url('touch/user/order_list')),
+    		);
+    		ecjia_front::$controller->assign('url', $url);
     		ecjia_front::$controller->assign('order_type', $order_type);
     	}
         ecjia_front::$controller->display('pay_notify.dwt', $cache_id);
