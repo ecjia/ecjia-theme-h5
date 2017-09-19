a47dc1b8a91e41e74ce69b2781e83ac3bde67d1a
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-19 15:23:47 +0800

    删除调试信息

--- js/ecjia.touch.js
+++ js/ecjia.touch.js
@@ -228,7 +228,6 @@
 	        		        dataUrl: '', 					// 如果type是music或video，则要提供数据链接，默认为空
 	        		        success: function () { 
 	        		            // 用户确认分享后执行的回调函数
-	        		        	alert(1);
 	        		        },
 	        		        cancel: function () { 
 	        		            // 用户取消分享后执行的回调函数
