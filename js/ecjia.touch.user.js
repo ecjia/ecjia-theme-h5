9302841f490404e12a6a4511ad1ed8d2a143ffd3
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-12-04 15:04:13 +0800

    修改js

--- js/ecjia.touch.user.js
+++ js/ecjia.touch.user.js
@@ -583,7 +583,10 @@
 			        	var col0 = picker.cols[0].container.find('.picker-selected');
 			        	var col1 = picker.cols[1].container.find('.picker-selected');
 			        	var col2 = picker.cols[2].container.find('.picker-selected');
-		        		var html = col0.html()+'-'+col1.html();
+		        		var html = col0.html();
+		        		if (col1.html() != '暂无') {
+		        			html += '-'+col1.html();
+		        		}
 		        		if (col2.html() != '暂无') {
 		        			html += '-'+col2.html();
 		        		}
