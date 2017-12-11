4ee26548906154eb845cfa38de544fbe5838a00d
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-12-11 11:06:32 +0800

    修改h5 js判断，提交.map文件

--- js/ecjia.touch.user.js
+++ js/ecjia.touch.user.js
@@ -550,7 +550,7 @@
 			if ($.localStorage('district') == undefined) {
 				$.localStorage('district', district);
 			}
-			var data = region_data();
+			var data = region_data('', '', '');
 			
 			var province_list 	= data[0];
 			var province_list_name 	= data[1];
@@ -581,7 +581,7 @@
 			            values: province_list,
 			            displayValues: province_list_name,
 			            onChange: function(picker, value) {
-		            		var data = region_data(value);
+		            		var data = region_data(value, '', '');
 		            		if (picker.cols[1].replaceValues) {
 		            			picker.cols[1].replaceValues(data[2], data[3]);
 		            		}
@@ -594,7 +594,7 @@
 			            values: city_list,
 			            displayValues: city_list_name,
 			            onChange: function(picker, value) {
-			            	var data = region_data('', value);
+			            	var data = region_data('', value, '');
 			            	if (picker.cols[2].replaceValues) {
 			            		picker.cols[2].replaceValues(data[4], data[5]);
 			            	}
@@ -633,7 +633,7 @@
 		        		$('input[name="district"]').val(col2Value);
 
 		        		if (district_value != col2Value) {
-		        			$('.ecjia_user_address_street_picker').html('');
+		        			$('.ecjia_user_address_street_picker').html('请选择街道');
 		        			$('input[name="street"]').val('');
 		        		}
 		        		$.post(url, {district_id:col2Value}, function(data) {
@@ -743,7 +743,7 @@
 	}
 
 	//处理地区数据
-	function region_data(province_id = '', city_id = '', district_id = '') {
+	function region_data(province_id, city_id, district_id) {
 		var province = eval($.localStorage('province'));
 		var city = eval($.localStorage('city'));
 		var district = eval($.localStorage('district'));
