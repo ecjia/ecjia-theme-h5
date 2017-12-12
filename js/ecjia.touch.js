5b34e46039a82d3c4c96823a1cdcbd0a1d57e8d4
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-12-12 11:06:59 +0800

    修改判断错误位置

--- js/ecjia.touch.js
+++ js/ecjia.touch.js
@@ -197,10 +197,6 @@
 	        				'hideOptionMenu',
 	        			]
 	        		});
-	        		wx.error(function(res){
-	        			console.log(res);
-	        		    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-	        		});
 	        		var title = $('input[name="share_title"]').val() == undefined ? document.title : '';
 	        		var image = $('input[name="share_image"]').val();
 	        		var desc = $('input[name="share_desc"]').val() == undefined ? document.title : '';
@@ -253,6 +249,10 @@
 	        		        }
 	        		    });
 	        		});	
+	        		wx.error(function(res){
+	        			console.log(res);
+	        		    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+	        		});
         		}
         	});
 		},
