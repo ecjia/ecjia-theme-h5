8d372e459a68868bf102f328a7c1b23f8671a818
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-01-11 14:13:15 +0800

    增加支付方式判断

--- extras/controller/cart_controller.php
+++ extras/controller/cart_controller.php
@@ -636,8 +636,13 @@ class cart_controller {
    		}
    		
    		if (empty($shipping_id)) {
-   			return ecjia_front::$controller->showmessage('请选择一个配送方式', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR, array('pjaxurl' => ''));
+   			return ecjia_front::$controller->showmessage('请选择一个配送方式', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
    		}
+   		
+   		if (empty($pay_id)) {
+   			return ecjia_front::$controller->showmessage('请选择一个支付方式', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+   		}
+   		
    		$params = array(
    			'token' 				=> ecjia_touch_user::singleton()->getToken(),
    			'address_id' 			=> $address_id,
