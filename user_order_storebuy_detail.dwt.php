2198c0b607f1f045a91534cd9423681d8a6b6c9c
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-01-15 14:06:52 +0800

    修改h5到店购物订单详情显示货号

--- user_order_storebuy_detail.dwt.php
+++ user_order_storebuy_detail.dwt.php
@@ -34,11 +34,7 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 					</div>
 					<div class="ecjiaf-fl goods-info">
 						<p class="ecjia-truncate2">{$goods.name}</p>
-						<p class="ecjia-goods-attr goods-attr">
-						<!-- {foreach from=$goods.goods_attr item=attr} -->
-						{if $attr.name}{$attr.name}:{$attr.value}{/if}
-						<!-- {/foreach} -->
-						</p>
+						<p class="ecjia-goods-attr goods-attr">货号：{$goods.goods_sn}</p>
 						<p class="ecjia-color-red goods-attr-price">{$goods.formated_shop_price}</p>
 					</div>
 					<span class="ecjiaf-fr goods-price"> x {$goods.goods_number}</span>
