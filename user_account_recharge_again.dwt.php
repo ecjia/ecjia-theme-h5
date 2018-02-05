cf5bf3ae3f0f9f2887ee14031b89f05aa6ee9e86
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-02-05 17:46:10 +0800

    更新参数

--- user_account_recharge_again.dwt.php
+++ user_account_recharge_again.dwt.php
@@ -32,8 +32,8 @@ ecjia.touch.user_account.init();
 		        <li class="ecjia-account-padding-input user_pay_way">
 		            <span class="icon-name {$list.pay_code}" data-code="{$list.pay_code}">
                 		<label for="{$list.pay_code}" class="ecjiaf-fr ecjia-check" value="10">
-                		<input type="radio" id="{$list.pay_code}" name="pay_code" value="{$list.pay_code}" {if $list.checked}checked="true"{/if}>
-                		<input type="hidden" name="pay_id" value="{$list.pay_id}" >
+                		<input type="radio" id="{$list.pay_code}" name="payment_id" value="{$list.pay_id}" {if $list.checked}checked="true"{/if}>
+                		<input type="hidden" name="pay_code" value="{$list.pay_code}" >
                 		</label>
 		            	{$list.pay_name}
 		            </span>
