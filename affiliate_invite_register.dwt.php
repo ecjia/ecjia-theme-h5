19691c1d87d892f66573d59fa94c2cf6145830fa
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-28 11:46:45 +0800

    修改分享页样式

--- affiliate_invite_register.dwt.php
+++ affiliate_invite_register.dwt.php
@@ -17,36 +17,42 @@ defined('IN_ECJIA') or header("HTTP/1.0 404 Not Found");exit('404 Not Found');
 
 <!-- {block name="main-content"} -->
 <div class="ecjia-invite-register">
-	<img style="width: 100%;" src="{$theme_url}images/invite.png">
-	<div class="form">
-		<form class="invite-form" name="inviteForm" action='{url path="affiliate/index/invite"}'>
-			<div class="input-container">
-				<input type="text" id="mobile" name="mobile" placeholder="输入手机号码"/>
-			</div>
-			<div class="input-container">
-				<span class="identify_code" data-url="{url path='affiliate/index/refresh'}"><img src="data:image/png;base64,{$captcha_image}"></span>
-				<input class="code_captcha" type="text" name="code_captcha" placeholder="请输入左侧验证码"/>
-				<span class="identify_code_btn" data-url="{url path='affiliate/index/check'}">验证</span>
-			</div>
-			<div class="input-container">
-				<input type="text" name="code" placeholder="输入短信验证码"/>
-			</div>
-			<input type="hidden" name="invite_code" value="{$invite_code}"/>
-			<input class="receive_btn" type="submit" value="立即领取"/>
-		</form>
+	<div class="invite-top" style="position: relative;">
+		<img style="width: 100%; height: 34em;" src="{$theme_url}images/invite_01.png">
+		<div class="form">
+			<form class="invite-form" name="inviteForm" action='{url path="affiliate/index/invite"}'>
+				<div class="input-container">
+					<input type="text" id="mobile" name="mobile" placeholder="输入手机号码"/>
+				</div>
+				<div class="input-container">
+					<span class="identify_code" data-url="{url path='affiliate/index/refresh'}"><img src="data:image/png;base64,{$captcha_image}"></span>
+					<input class="code_captcha" type="text" name="code_captcha" placeholder="请输入左侧验证码"/>
+					<span class="identify_code_btn" data-url="{url path='affiliate/index/check'}">验证</span>
+				</div>
+				<div class="input-container">
+					<input type="text" name="code" placeholder="输入短信验证码"/>
+				</div>
+				<input type="hidden" name="invite_code" value="{$invite_code}"/>
+				<input class="receive_btn" type="submit" value="立即领取"/>
+			</form>
+		</div>
 	</div>
-	<div class="invite-notice">
-		<ul>
-			<!--{foreach from=$invite_user.invitee_rule_explain item=invite}-->
-			{if $invite}
-			<li>
-				<p>
-					{$invite}
-				</p>
-			</li>
-			{/if}
-			<!--{/foreach}-->
-		</ul>
+	
+	<div class="invite-bottom" style="position: relative; height: 19em;">
+		<img style="width: 100%;height: 5.4em;" src="{$theme_url}images/invite_02.png">
+		<div class="invite-notice">
+			<ul>
+				<!--{foreach from=$invite_user.invitee_rule_explain item=invite}-->
+				{if $invite}
+				<li>
+					<p>
+						{$invite}
+					</p>
+				</li>
+				{/if}
+				<!--{/foreach}-->
+			</ul>
+		</div>
 	</div>
 </div>
 <!-- {/block} -->
