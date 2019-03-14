ad28bbbac301ab3b5ed8863592da6e64ac75bd3d
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-14 17:58:46 +0800

    优化H5的CONNECT_SIGNIN接口请求

--- functions.php
+++ functions.php
@@ -199,7 +199,10 @@ RC_Hook::add_action('connect_callback_user_signin', function($connect_user) {
 //    );
 //    ecjia_touch_user::singleton()->setUserinfo($res);
 
-    $connect_signin = ecjia_touch_user::singleton()->connectSignin($connect_user->getOpenId(), $connect_user->getConnectCode());
+    /**
+     * @var \Ecjia\App\Connect\ConnectUser\ConnectUser $connect_user
+     */
+    $connect_signin = ecjia_touch_user::singleton()->connectSignin($connect_user->getConnectCode(), $connect_user->getOpenId(), $connect_user->getUnionId(), $connect_user->getProfile());
     if (is_ecjia_error($connect_signin)) {
         return ecjia_front::$controller->showmessage($connect_signin->get_error_message(), ecjia::MSGTYPE_ALERT | ecjia::MSGSTAT_ERROR);
     }
