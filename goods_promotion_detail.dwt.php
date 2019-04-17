91ca1e257dec82dcfa52020913636448747c623e
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-17 14:59:13 +0800

    商品祥情的相关商品移动至独立的library文件里

--- goods_promotion_detail.dwt.php
+++ goods_promotion_detail.dwt.php
@@ -234,50 +234,7 @@ var releated_goods = {$releated_goods};
 			</div>
 			{/if}
 
-			<!-- {if $goods_info.related_goods} -->
-			<div class="address-warehouse ecjia-margin-t address-warehouse-new">
-				<div class="ecjia-form">
-					<div class="may-like-literal"><span class="may-like">{t domain="h5"}猜你喜欢{/t}</span></div>
-				</div>
-				<div class="ecjia-margin-b form-group ecjia-form">
-					<div class="bd">
-						<ul class="ecjia-list ecjia-like-goods-list">
-							<!--{foreach from=$goods_info.related_goods item=goods name=goods}-->
-							<!-- {if $smarty.foreach.goods.index < 6 } -->
-							<li>
-								<a class="nopjax external" href='{url path="goods/index/show" args="goods_id={$goods.goods_id}"}'>
-									<span class="like-goods-img">
-										<img src="{$goods.img.url}" alt="{$goods.name}" title="{$goods.name}" /></span>
-								</a>
-								<p class="link-goods-name ecjia-goods-name-new">{$goods.name}</p>
-								<div class="link-goods-price">
-									<!--{if $goods.promote_price}-->
-									<span class="goods-price">{$goods.promote_price}</span>
-									<!--{else}-->
-									<span class="goods-price">{$goods.shop_price}</span>
-									<!--{/if}-->
-
-									{if $goods_info.shop_closed neq 1}
-										{if $goods.specification}
-										<div class="goods_attr goods_spec_{$goods.goods_id}" goods_id="{$goods_info.id}">
-											<span class="choose_attr spec_goods" rec_id="{$goods.rec_id}" goods_id="{$goods.goods_id}" data-num="{$goods.num}"
-											data-spec="{$goods.default_spec}" data-url="{RC_Uri::url('cart/index/check_spec')}" data-store="{$store_id}">{t domain="h5"}选规格{/t}</span>
-											{if $goods.num}<i class="attr-number">{$goods.num}</i>{/if}
-										</div>
-										{else}
-										<span class="goods-price-plus may_like_{$goods.goods_id}" data-toggle="add-to-cart" rec_id="{$goods.rec_id}"
-										goods_id="{$goods.goods_id}" data-num="{$goods.num}"></span>
-										{/if}
-									{/if}
-								</div>
-							</li>
-							<!--{/if}-->
-							<!--{/foreach}-->
-						</ul>
-					</div>
-				</div>
-			</div>
-			<!-- {/if} -->
+            <!-- #BeginLibraryItem "/library/goods_related_goods.lbi" --><!-- #EndLibraryItem -->
 		</div>
 	</form>
 </div>
