4fdb8b6bf5917aa14fe194bc37f1eb25296cf839
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-17 13:34:00 +0800

    商品祥情页接口返回数据id = goods_id + product_id

--- extras/controller/goods_controller.php
+++ extras/controller/goods_controller.php
@@ -182,6 +182,8 @@ class goods_controller
                 }
             }
 
+            $goods_info['id'] = $goods_info['goods_id'] . '_' . $goods_info['product_id'];
+
             //店铺信息
             $parameter_list = array(
                 'seller_id' => $goods_info['seller_id'],
