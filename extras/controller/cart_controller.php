a35824647dc294b0d24506623089e16498b8a307
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-18 13:35:46 +0800

    清理代码

--- extras/controller/cart_controller.php
+++ extras/controller/cart_controller.php
@@ -449,84 +449,13 @@ class cart_controller
             $spec = implode('|', $spec);
         }
 
-
-//        $spec = !empty($_POST['spec']) ? $_POST['spec'] : '';
-
-
-//        $goods_id = !empty($_POST['goods_id']) ? intval($_POST['goods_id']) : 0;
-//        $store_id = !empty($_POST['store_id']) ? intval($_POST['store_id']) : (!empty($_GET['store_id']) ? intval($_GET['store_id']) : 0);
-
         list($goods_id, $product_id) = explode('_', $goods_id);
         $goods_id   = intval($goods_id);
-//        $product_id = intval($product_id);
-
-
-
-//        $token = ecjia_touch_user::singleton()->getToken();
-//        $arr   = array(
-//            'token'     => $token,
-//            'seller_id' => $seller_id,
-//            'location'  => array('longitude' => $_COOKIE['longitude'], 'latitude' => $_COOKIE['latitude']),
-//            'city_id'   => $_COOKIE['city_id'],
-//        );
-
-        //店铺购物车商品
-//        $ecjia_cart = new ecjia_cart($store_id);
-//        $cart_list  = $ecjia_cart->getLocalStorage();
-
-//        $cart_list = RC_Cache::app_cache_get('cart_goods' . $token . $seller_id . $_COOKIE['longitude'] . $_COOKIE['latitude'] . $_COOKIE['city_id'], 'cart');
-
-//        if (empty($cart_list['cart_list'])) {
-//            $cart_list = ecjia_touch_manager::make()->api(ecjia_touch_api::CART_LIST)->data($arr)->run();
-//            if (is_ecjia_error($cart_list)) {
-//                return ecjia_front::$controller->showmessage($cart_list->get_error_message(), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
-//            }
-//            RC_Cache::app_cache_set('cart_goods' . $token . $seller_id . $_COOKIE['longitude'] . $_COOKIE['latitude'] . $_COOKIE['city_id'], $cart_list, 'cart');
-//        }
 
         $ecjia_goods_specification = new ecjia_goods_specification($goods_id);
 
         $product_spec = $ecjia_goods_specification->findProductSpecificationBySpec($spec);
-
-//        $goods_specification = $ecjia_goods_specification->getLocalStorage();
-//        $product_specification = $goods_specification['product_specification'];
-
-//        $product_specification = RC_Cache::app_cache_get(sprintf('%X', crc32('goods_product_specification_' . $goods_id)), 'goods');
-
-//        $product_id = 0;
-//        if (!empty($product_specification)) {
-//            foreach ($product_specification as $key => $value) {
-//                if (!empty($value['product_goods_attr'])) {
-////                    $goods_attr = explode('|', $value['product_goods_attr']);
-////                    asort($goods_attr);
-////                    $goods_attr = implode(',', $goods_attr);
-//                    if ($spec == $value['product_goods_attr'] && !empty($value['product_id'])) {
-//                        $product_id = $value['product_id'];
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-
-//        if (!empty($cart_list['cart_list'][0]['goods_list'])) {
-//            foreach ($cart_list['cart_list'][0]['goods_list'] as $key => $val) {
-//                if ($goods_id == $val['goods_id']) {
-//                    if (empty($val['goods_attr_id']) && empty($spec)) {
-//                        return ecjia_front::$controller->showmessage('', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS, array('info' => $val, 'product_id' => $product_id));
-//                    } else {
-//                        $goods_attr = explode(',', $val['goods_attr_id']);
-//                        if (!empty($goods_attr)) {
-//                            asort($goods_attr);
-//                            $goods_attr = implode(',', $goods_attr);
-//                            if ($spec == $goods_attr) {
-//                                return ecjia_front::$controller->showmessage('', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS, array('info' => $val, 'product_id' => $product_id));
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-
+        
         $product_id = $product_spec['product_id'];
         $id = $goods_id . '_' . $product_id;
 
