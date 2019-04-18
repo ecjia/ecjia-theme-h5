3a30328a76e4b34fc4ce9eff5238cf53e7c2c113
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-18 19:39:13 +0800

    调试

--- js/ecjia.touch.goods.js
+++ js/ecjia.touch.goods.js
@@ -598,8 +598,10 @@
 						$('.ecjia-attr-modal').find('#goods_' + goods_id).removeClass('show').addClass('hide').children().attr('rec_id', '');
 					} else {
 						$('.ecjia-attr-modal').find('.add-tocart').removeClass('show').addClass('hide');
-						$('.ecjia-attr-modal').find('#goods_' + goods_id).addClass('show').removeClass('hide');
-						$('.ecjia-attr-modal').find('#goods_' + goods_id).children().addClass('show').removeClass('hide');
+						// $('.ecjia-attr-modal').find('#goods_' + goods_id).addClass('show').removeClass('hide');
+						// $('.ecjia-attr-modal').find('#goods_' + goods_id).children().addClass('show').removeClass('hide');
+						$('.ecjia-attr-modal').find('.ecjia-choose-attr-box.box').addClass('show').removeClass('hide');
+						$('.ecjia-attr-modal').find('.ecjia-choose-attr-box.box').children().addClass('show').removeClass('hide');
 					}
 					$('.ecjia-attr-modal').find('#goods_' + goods_id).children('label').html(val);
 				}
