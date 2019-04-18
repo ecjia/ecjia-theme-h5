5e9b6ab5cb6c598b28928d9c80c4d5578ad16112
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-18 10:31:08 +0800

    调试商家商品列表不带货品显示效果

--- extras/controller/goods_controller.php
+++ extras/controller/goods_controller.php
@@ -186,7 +186,7 @@ class goods_controller
                 }
             }
 
-//            dd($goods_info);
+            dd($goods_info);
             $goods_info['id'] = $goods_info['goods_id'] . '_' . $goods_info['product_id'];
 
             //店铺信息
@@ -210,7 +210,6 @@ class goods_controller
                 $product_info = [];
                 if (!empty($goods_info['product_specification'])) {
 
-                    $ecjia_goods_specification = new ecjia_goods_specification($goods_id);
                     $ecjia_goods_specification->saveLocalStorage($goods_info['product_specification']);
 //                    RC_Cache::app_cache_set(sprintf('%X', crc32('goods_product_specification_' . $goods_id)), $goods_info['product_specification'], 'goods');
     
