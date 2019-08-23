9400d66892720d2843f0f77fcde5aa3de5e8b375
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-08-23 12:08:26 +0800

    收款页面测试

--- js/ecjia.touch.quickpay.js
+++ js/ecjia.touch.quickpay.js
@@ -7,7 +7,7 @@
 		init: function () {
 			document.body.addEventListener('touchmove', function (e) {
 				e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-			}, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+			}, {passive: true}); //passive 参数不能省略，用来兼容ios和android
 
 			$("body").greenCheck();
 			ecjia.touch.quickpay.checkbtn();
